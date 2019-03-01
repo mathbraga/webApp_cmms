88dc4ea7c5da42b53fc2b82ee0d33ccab9f3f6d0
hzlopes <hzlopes@senado.leg.br>
2019-03-01 13:14:27 -0300

    Create components Chart and SimpleTable

--- src/containers/Water/Water.jsx
+++ src/containers/Water/Water.jsx
@@ -1,9 +1,26 @@
 import React, { Component, Suspense } from "react";
 import FormDates from "../../components/Forms/FormDates";
+import Chart from "../../components/Charts/Chart";
+import SimpleTable from "../../components/Tables/SimpleTable";
+import { CardColumns } from "reactstrap";
 
 class Water extends Component {
   render() {
-    return <FormDates />;
+    return(
+      <div>
+        
+        <FormDates />
+        
+        <CardColumns className="cols-2">
+
+          <Chart />
+
+          <SimpleTable />
+
+        </CardColumns>
+      
+      </div>
+    );
   }
 }
 
