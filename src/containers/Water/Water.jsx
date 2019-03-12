36436bebb82aeb5737498ab5110afea8cf604f3d
Pedro Henrique Serafim <serafaobr@gmail.com>
2019-03-12 14:56:27 -0300

    Add handle dates to water

--- src/containers/Water/Water.jsx
+++ src/containers/Water/Water.jsx
@@ -3,12 +3,30 @@ import FormDates from "../../components/Forms/FormDates";
 import Chart from "../../components/Charts/Chart";
 import SimpleTable from "../../components/Tables/SimpleTable";
 import { CardColumns } from "reactstrap";
+import { handleDates } from "../../utils/handleDates";
 
 class Water extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: "",
+      options: "",
+      initialDate: "",
+      finalDate: "",
+      consumerUnit: ""
+    };
+  }
+
+  handleChangeOnDates = handleDates.bind(this);
+
   render() {
     return (
       <div>
-        <FormDates />
+        <FormDates
+          onChange={this.handleChangeOnDates}
+          initialDate={this.state.initialDate}
+          finalDate={this.state.finalDate}
+        />
         <CardColumns className="cols-2">
           <Chart />
           <SimpleTable />
