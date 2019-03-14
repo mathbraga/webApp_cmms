faa014fd5d6c1eb84806069b0810bdf933188e4c
Pedro Henrique Serafim <serafaobr@gmail.com>
2019-03-14 17:04:59 -0300

    Create widget for energy demand

---
+++ src/components/Widgets/WidgetEnergyDemand.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Card, CardBody, Col, Row } from "reactstrap";
+
+class WidgetEnergyDemand extends Component {
+  state = {};
+  render() {
+    return (
+      <Card className="widget-container">
+        <CardBody className="widget-body">
+          <Row>
+            <Col md="3">
+              <div>
+                <div className="widget-title">Demanda P</div>
+                <div>100 kW</div>
+                <div className="widget-title" style={{ "padding-top": "5px" }}>
+                  Demanda FP
+                </div>
+                <div>120 kW</div>
+              </div>
+            </Col>
+            <Col md="3">
+              <div className="widget-division">
+                <div className="widget-title">Contratado P</div>
+                <div>150 kW</div>
+                <div className="widget-title" style={{ "padding-top": "5px" }}>
+                  Contratado FP
+                </div>
+                <div>180 kW</div>
+              </div>
+            </Col>
+            <Col md="3">
+              <div className="widget-division">
+                <div className="widget-title">Faturado P</div>
+                <div>150 kW</div>
+                <div className="widget-title" style={{ "padding-top": "5px" }}>
+                  Faturado FP
+                </div>
+                <div>180 kW</div>
+              </div>
+            </Col>
+            <Col md="3" className="widget-container-image">
+              <div>
+                <img
+                  className="widget-image"
+                  src={require("../../assets/icons/iconfinder_a_5_2578124.png")}
+                />
+              </div>
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
+    );
+  }
+}
+
+export default WidgetEnergyDemand;
