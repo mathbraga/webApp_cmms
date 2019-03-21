517fa032187517a5baab78084f3230547122b4fe
hzlopes <hzlopes@senado.leg.br>
2019-03-21 12:55:50 -0300

    Set finalDate = initialDate after checkbox OneMonth is set to true

--- src/utils/queryEnergyTable.js
+++ src/utils/queryEnergyTable.js
@@ -2,12 +2,12 @@ export function queryEnergyTable(state, tableName) {
   return new Promise((resolve, reject) => {
     // Transform form inputs into integers
     var month1 = state.initialDate.slice(5) + state.initialDate.slice(0, 2);
-    var month2 = "";
-    if (state.oneMonth) {
-      month2 = month1;
-    } else {
-      month2 = state.finalDate.slice(5) + state.finalDate.slice(0, 2);
-    }
+    var month2 = state.finalDate.slice(5) + state.finalDate.slice(0, 2);
+    // if (state.oneMonth) {
+    //   month2 = month1;
+    // } else {
+    //   month2 = state.finalDate.slice(5) + state.finalDate.slice(0, 2);
+    // }
 
     // Check if consumer is 'all'
     if(state.chosenMeter === "199") {
@@ -44,11 +44,11 @@ export function queryEnergyTable(state, tableName) {
                 element[key] = Number(element[key].N);
               });
             });
-            resultAll.push(data.Items);
+            resultAll.push(data);    
           }
-          resolve(resultAll);
-        })    
+        })
       })
+      resolve(resultAll);
 
     } else {
       
@@ -69,7 +69,6 @@ export function queryEnergyTable(state, tableName) {
         }
       }, (err, data) => {
         if (err) {
-          console.log(err);
           alert("There was an error. Please insert search parameters again.");
           reject(Error("Failed to get the items."));
         } else {
