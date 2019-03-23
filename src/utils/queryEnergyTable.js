d5e5b2d1f23941981b179f91a9a2f588bdb48683
hzlopes <hzlopes@gmail.com>
2019-03-23 14:12:28 -0300

    Remove query for one meter, add conditional for allMeters value

--- src/utils/queryEnergyTable.js
+++ src/utils/queryEnergyTable.js
@@ -1,3 +1,5 @@
+// import buildChartData from './buildChartData';
+
 export function queryEnergyTable() {
   // Transform form inputs into integers
   var month1 = this.state.initialDate.slice(5) + this.state.initialDate.slice(0, 2);
@@ -9,112 +11,62 @@ export function queryEnergyTable() {
   }
 
   // Check if consumer is 'all'
+  var allMeters = [];
   if (this.state.chosenMeter === "199") {
     // Build array of all meters to query
-    var allMeters = this.state.meters.map(meter => {
+    allMeters = this.state.meters.map(meter => {
       return (
         100 * parseInt(meter.medtype.N, 10) +
         parseInt(meter.med.N, 10)
       ).toString();
     });
-
-    // Query all meters in chosen period
-    var resultAll = [];
-    let arrayPromises = allMeters.map(meter => {
-      return new Promise((resolve, reject) => {
-        this.state.dynamo.query(
-          {
-            TableName: this.state.tableName,
-            KeyConditionExpression:
-              "med = :med AND aamm BETWEEN :aamm1 AND :aamm2",
-            ExpressionAttributeValues: {
-              ":med": {
-                N: meter
-              },
-              ":aamm1": {
-                N: month1
-              },
-              ":aamm2": {
-                N: month2
-              }
-            }
-          },
-          (err, data) => {
-            if (err) {
-              alert(
-                "There was an error. Please insert search parameters again."
-              );
-            } else {
-              data.Items.map(element => {
-                Object.keys(element).map(key => {
-                  element[key] = Number(element[key].N);
-                });
-              });
-              resultAll.push(data);
-            }
-            resolve();
-          }
-        );
-      });
-    });
-    // console.log(arrayPromises);
-    Promise.all(arrayPromises).then(() => {
-      this.setState({
-        queryResponse: resultAll,
-        showResult: true,
-        error: false
-      });
-    });
-  
   } else {
-    
-    // Query for only one meter
-    let resultOne = [];
-    let oneMeter = [this.this.state.chosenMeter];
-    let arrayPromises = oneMeter.map(meter => {
-      return new Promise((resolve, reject) => {
-        this.state.dynamo.query(
-          {
-            TableName: this.state.tableName,
-            KeyConditionExpression:
-              "med = :med AND aamm BETWEEN :aamm1 AND :aamm2",
-            ExpressionAttributeValues: {
-              ":med": {
-                N: meter
-              },
-              ":aamm1": {
-                N: month1
-              },
-              ":aamm2": {
-                N: month2
-              }
-            }
-          },
-          (err, data) => {
-            if (err) {
-              alert(
-                "There was an error. Please insert search parameters again."
-              );
-            } else {
-              data.Items.map(element => {
-                Object.keys(element).map(key => {
-                  element[key] = Number(element[key].N);
-                });
-              });
-              resultOne.push(data);
-            }
-            resolve();
-          }
-        );
-      });
-    });
-    // console.log(arrayPromises);
-    Promise.all(arrayPromises).then(() => {
-      this.setState({
-        queryResponse: resultOne,
-        showResult: true,
-        error: false
-      });
-    });
+    allMeters = [this.state.chosenMeter];
   }
+  // Query all chosen meters
+  let queryResponse = [];
+  let arrayPromises = allMeters.map(meter => {
+    return new Promise((resolve, reject) => {
+      this.state.dynamo.query(
+        {
+          TableName: this.state.tableName,
+          KeyConditionExpression:
+            "med = :med AND aamm BETWEEN :aamm1 AND :aamm2",
+          ExpressionAttributeValues: {
+            ":med": {
+              N: meter
+            },
+            ":aamm1": {
+              N: month1
+            },
+            ":aamm2": {
+              N: month2
+            }
+          }
+        }, (err, data) => {
+          if (err) {
+            alert("There was an error. Please insert search parameters again.");
+            reject();
+          } else {
+            data.Items.map(element => {
+              Object.keys(element).map(key => {
+                element[key] = Number(element[key].N);
+              });
+            });
+            queryResponse.push(data);
+          }
+          resolve();
+        }
+      );
+    });
+  });
+  console.log(queryResponse);
+  Promise.all(arrayPromises).then(() => {
+    // buildChartData(queryResponse);
+    this.setState({
+      queryResponse: queryResponse,
+      showResult: true,
+      error: false
+    });
+  });
 }
