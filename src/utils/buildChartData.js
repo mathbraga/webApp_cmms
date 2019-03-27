0b687d1e8324f920d72697894f539ff2f71d63a9
hzlopes <hzlopes@senado.leg.br>
2019-03-27 14:29:47 -0300

    Fix bug in loop in buildChartData (add check for length in each meter Items array)

--- src/utils/buildChartData.js
+++ src/utils/buildChartData.js
@@ -32,30 +32,32 @@ export default function buildChartData(queryResponse, month1, month2){
     answers.basec.push(0); answers.cip.push(0); answers.desc.push(0); answers.dff.push(0); answers.dfp.push(0); answers.dmf.push(0); answers.dmp.push(0); answers.erexf.push(0); answers.erexp.push(0); answers.icms.push(0); answers.jma.push(0); answers.kwh.push(0); answers.kwhf.push(0); answers.kwhp.push(0); answers.trib.push(0); answers.vbru.push(0); answers.vdff.push(0); answers.vdfp.push(0); answers.verexf.push(0); answers.verexp.push(0); answers.vliq.push(0); answers.vudf.push(0); answers.vudp.push(0);
     for(let j = 0; j <= numMeters - 1; j++){
       if(queryResponse[j].Items.length > 0){ // Se o medidor não retornou dados (queryResponseay vazio); não será analisado
-        if(queryResponse[j].Items[i].aamm === periodInts[i]){ // Se o mês corresponde ao pesquisado, podemos somá-lo
-          answers.basec[i] = answers.basec[i] + queryResponse[j].Items[i].basec;
-          answers.cip[i]   = answers.cip[i] + queryResponse[j].Items[i].cip;
-          answers.desc[i]  = answers.desc[i] + queryResponse[j].Items[i].desc;
-          answers.dff[i]   = answers.dff[i] + queryResponse[j].Items[i].dff;
-          answers.dfp[i]   = answers.dfp[i] + queryResponse[j].Items[i].dfp;
-          answers.dmf[i]   = answers.dmf[i] + queryResponse[j].Items[i].dmf;
-          answers.dmp[i]   = answers.dmp[i] + queryResponse[j].Items[i].dmp;
-          answers.erexf[i] = answers.erexf[i] + queryResponse[j].Items[i].erexf;
-          answers.erexp[i] = answers.erexp[i] + queryResponse[j].Items[i].erexp;
-          answers.icms[i]  = answers.icms[i] + queryResponse[j].Items[i].icms;
-          answers.jma[i]   = answers.jma[i] + queryResponse[j].Items[i].jma;
-          answers.kwh[i]   = answers.kwh[i] + queryResponse[j].Items[i].kwh;
-          answers.kwhf[i]  = answers.kwhf[i] + queryResponse[j].Items[i].kwhf;
-          answers.kwhp[i]  = answers.kwhp[i] + queryResponse[j].Items[i].kwhp;
-          answers.trib[i]  = answers.trib[i] + queryResponse[j].Items[i].trib;
-          answers.vbru[i]  = answers.vbru[i] + queryResponse[j].Items[i].vbru;
-          answers.vdff[i]  = answers.vdff[i] + queryResponse[j].Items[i].vdff;
-          answers.vdfp[i]  = answers.vdfp[i] + queryResponse[j].Items[i].vdfp;
-          answers.verexf[i] = answers.verexf[i] + queryResponse[j].Items[i].verexf;
-          answers.verexp[i] = answers.verexp[i] + queryResponse[j].Items[i].verexp;
-          answers.vliq[i]   = answers.vliq[i] + queryResponse[j].Items[i].vliq;
-          answers.vudf[i]   = answers.vudf[i] + queryResponse[j].Items[i].vudf;
-          answers.vudp[i]   = answers.vudp[i] + queryResponse[j].Items[i].vudp;
+        for(let k = 0; k <= queryResponse[j].Items.length - 1; k++){
+          if(queryResponse[j].Items[k].aamm === periodInts[i]){ // Se o mês corresponde ao pesquisado, podemos somá-lo
+            answers.basec[i] = answers.basec[i] + queryResponse[j].Items[k].basec;
+            answers.cip[i]   = answers.cip[i] + queryResponse[j].Items[k].cip;
+            answers.desc[i]  = answers.desc[i] + queryResponse[j].Items[k].desc;
+            answers.dff[i]   = answers.dff[i] + queryResponse[j].Items[k].dff;
+            answers.dfp[i]   = answers.dfp[i] + queryResponse[j].Items[k].dfp;
+            answers.dmf[i]   = answers.dmf[i] + queryResponse[j].Items[k].dmf;
+            answers.dmp[i]   = answers.dmp[i] + queryResponse[j].Items[k].dmp;
+            answers.erexf[i] = answers.erexf[i] + queryResponse[j].Items[k].erexf;
+            answers.erexp[i] = answers.erexp[i] + queryResponse[j].Items[k].erexp;
+            answers.icms[i]  = answers.icms[i] + queryResponse[j].Items[k].icms;
+            answers.jma[i]   = answers.jma[i] + queryResponse[j].Items[k].jma;
+            answers.kwh[i]   = answers.kwh[i] + queryResponse[j].Items[k].kwh;
+            answers.kwhf[i]  = answers.kwhf[i] + queryResponse[j].Items[k].kwhf;
+            answers.kwhp[i]  = answers.kwhp[i] + queryResponse[j].Items[k].kwhp;
+            answers.trib[i]  = answers.trib[i] + queryResponse[j].Items[k].trib;
+            answers.vbru[i]  = answers.vbru[i] + queryResponse[j].Items[k].vbru;
+            answers.vdff[i]  = answers.vdff[i] + queryResponse[j].Items[k].vdff;
+            answers.vdfp[i]  = answers.vdfp[i] + queryResponse[j].Items[k].vdfp;
+            answers.verexf[i] = answers.verexf[i] + queryResponse[j].Items[k].verexf;
+            answers.verexp[i] = answers.verexp[i] + queryResponse[j].Items[k].verexp;
+            answers.vliq[i]   = answers.vliq[i] + queryResponse[j].Items[k].vliq;
+            answers.vudf[i]   = answers.vudf[i] + queryResponse[j].Items[k].vudf;
+            answers.vudp[i]   = answers.vudp[i] + queryResponse[j].Items[k].vudp;
+          }
         }
       }
     }
