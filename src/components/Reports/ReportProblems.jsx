f2ee79f3103007ca644fae3e94127ca901875db2
hzlopes <hzlopes@senado.leg.br>
2019-04-08 12:02:30 -0300

    Add badge with tooltips to table inside modal (ReportProblems)

--- src/components/Reports/ReportProblems.jsx
+++ src/components/Reports/ReportProblems.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import TooltipBadge from "../Tooltips/TooltipBadge";
+import { formatNumber } from "../../utils/formatText";
 import {
   Card,
   CardBody,
@@ -16,100 +18,92 @@ import {
   PopoverBody
 } from "reactstrap";
 
-const rowNames = {
-  dcp: {
-    name: "Demanda contratada - Ponta",
-    obs: "Maior que zero somente na modalidade tarifária horária Azul. Igual a zero nos outros casos.",
-    expected: "≥ 0 kW"
-  },
-  dcf: {
-    name: "Demanda contratada - Fora de ponta",
-    obs: "Igual a zero somente na modalidade tarirária convencional",
-    expected: "≥ 0 kW"
-  },
-  dmp: {
-    name: "Demanda medida - Ponta",
-    obs: "Maior demanda de potência ativa registrada no período - Ponta",
-    expected: "≤ Demanda contratada (Ponta)"
-  },
-  dmf: {
-    name: "Demanda medida - Fora de ponta",
-    obs: "Maior demanda de potência ativa registrada no período - Fora de ponta",
-    expected: "≤ Demanda contratada (Fora de ponta)"
-  },
-  dfp: {
-    name: "Demanda faturada - Ponta",
-    obs: "Demanda considerada no faturamento (maior valor entre medida e contratada) - Ponta",
-    expected: "≤ Demanda contratada (Ponta)"
-  },
-  dff: {
-    name: "Demanda faturada - Fora de ponta",
-    obs: "Demanda considerada no faturamento (maior valor entre medida e contratada) - Fora de ponta",
-    expected: "≤ Demanda contratada (Fora de ponta)"
-  },
-
-  vudp: {
-    name: "Custo da ultrapassagem de demanda - Ponta",
-    obs: "Valor adicional em caso de demanda medida superior à demanda contratada",
-    expected: "= R$ 0"
-  },
-
-  vudf: {
-    name: "Custo da ultrapassagem de demanda - Fora de ponta",
-    obs: "Valor adicional em caso de demanda medida superior à demanda contratada",
-    expected: "= R$ 0"
-  },
-
-  verexp: {
-    name: "Custo do EREX - Ponta",
-    obs: "Valor adicional em caso de excedentes de energia reativa (fator de potência inferior a 0,92)",
-    expected: "= R$ 0" },
-
-  verexf: {
-    name: "Custo do EREX - Fora de ponta",
-    obs: "Valor adicional em caso de excedentes de energia reativa (fator de potência inferior a 0,92)",
-    expected: "= R$ 0" },
-
-  jma: {
-    name: "Multas, juros e atualização monetária",
-    obs: "Valores adicionais decorrentes do atraso no pagamento de faturas anteriores",
-    expected: "= R$ 0"
-  },
-  desc: {
-    name: "Descontos e compensações",
-    obs: "Total de descontos e compensações devido a baixos indicadores de qualidade do serviço, conforme normas da ANEEL, ou correções de valores cobrados indevidamente em faturas anteriores",
-    expected: "= R$ 0"
-  }
-};
-
 class ReportProblems extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      openPopovers: {
-        dcp: false,
-        dcf: false,
-        dmp: false,
-        dmf: false,
-        dfp: false,
-        dff: false,
-        vudp: false,
-        vudf: false,
-        verexp: false,
-        verexf: false,
-        jma: false,
-        desc: false
-      }
-    }
-  }
-
-  togglePO(){
-    this.setState(prevState => {
-      prevState
-    });
+    this.state = {}
   }
 
   render() {
+
+    const rowNames = {
+      dcp: {
+        name: "Demanda contratada - Ponta",
+        unit: "kW",
+        obs: "Maior que zero somente na modalidade tarifária horária Azul. Igual a zero nos outros casos.",
+        expected: "≥ 0 kW"
+      },
+      dcf: {
+        name: "Demanda contratada - Fora de ponta",
+        unit: "kW",
+        obs: "Igual a zero somente na modalidade tarirária convencional",
+        expected: "≥ 0 kW"
+      },
+      dmp: {
+        name: "Demanda medida - Ponta",
+        unit: "kW",
+        obs: "Maior demanda de potência ativa registrada no período - Ponta",
+        expected: "≤ Demanda contratada (Ponta)"
+      },
+      dmf: {
+        name: "Demanda medida - Fora de ponta",
+        unit: "kW",
+        obs: "Maior demanda de potência ativa registrada no período - Fora de ponta",
+        expected: "≤ Demanda contratada (Fora de ponta)"
+      },
+      dfp: {
+        name: "Demanda faturada - Ponta",
+        unit: "kW",
+        obs: "Demanda considerada no faturamento (maior valor entre medida e contratada) - Ponta",
+        expected: "≤ Demanda contratada (Ponta)"
+      },
+      dff: {
+        name: "Demanda faturada - Fora de ponta",
+        unit: "kW",
+        obs: "Demanda considerada no faturamento (maior valor entre medida e contratada) - Fora de ponta",
+        expected: "≤ Demanda contratada (Fora de ponta)"
+      },
+    
+      vudp: {
+        name: "Custo da ultrapassagem de demanda - Ponta",
+        unit: "R$",
+        obs: "Valor adicional em caso de demanda medida superior à demanda contratada",
+        expected: "= R$ 0"
+      },
+    
+      vudf: {
+        name: "Custo da ultrapassagem de demanda - Fora de ponta",
+        unit: "R$",
+        obs: "Valor adicional em caso de demanda medida superior à demanda contratada",
+        expected: "= R$ 0"
+      },
+    
+      verexp: {
+        name: "Custo do EREX - Ponta",
+        unit: "R$",
+        obs: "Valor adicional em caso de excedentes de energia reativa (fator de potência inferior a 0,92)",
+        expected: "= R$ 0" },
+    
+      verexf: {
+        name: "Custo do EREX - Fora de ponta",
+        unit: "R$",
+        obs: "Valor adicional em caso de excedentes de energia reativa (fator de potência inferior a 0,92)",
+        expected: "= R$ 0" },
+    
+      jma: {
+        name: "Multas, juros e atualização monetária",
+        unit: "R$",
+        obs: "Valores adicionais decorrentes do atraso no pagamento de faturas anteriores",
+        expected: "= R$ 0"
+      },
+      desc: {
+        name: "Descontos e compensações",
+        unit: "R$",
+        obs: "Total de descontos e compensações devido a baixos indicadores de qualidade do serviço, conforme normas da ANEEL, ou correções de valores cobrados indevidamente em faturas anteriores",
+        expected: "= R$ 0"
+      }
+    };
+    
     return (
       <Modal
         isOpen={this.props.isOpen}
@@ -123,43 +117,39 @@ class ReportProblems extends Component {
           <Table bordered>
             <thead>
               <tr>
-                <th>Parâmetro</th>
-                <th>Valor registrado</th>
-                {/* <th>Faixa de normalidade</th>
-                <th>Observações</th> */}
-                <th>Buttons with popovers</th>
+                <th style={{ "text-align": "center" }}>Parâmetro</th>
+                <th style={{ "text-align": "center" }}>Valor registrado</th>
+                <th style={{ "text-align": "center" }}>Diagnóstico</th>
               </tr>
             </thead>
             <tbody>
               {Object.keys(rowNames).map(row => (
                 <tr>
-
                   <th scope="row">{rowNames[row].name}</th>
-                  
-                  <td>
-                    {this.props.problems && this.props.problems[row].value}
+                  <td style={{ "text-align": "center" }}>
+                    {this.props.problems && formatNumber(this.props.problems[row].value)}
                   </td>
-                  {/* <td>{rowNames[row].expected}</td>
-                  <td>{rowNames[row].obs}</td> */}
-                  <td>
+                  <td style={{ "text-align": "center" }}>
                     {this.props.problems && this.props.problems[row].problem
                       ? (
-                        <div>
-                          <Button id={row} color="danger" type="button">VERIFICAR</Button>
-                          {/* <Popover placement="right" isOpen={true} target={row} toggle={""}>
-                            <PopoverHeader>Title</PopoverHeader>
-                            <PopoverBody>Text</PopoverBody>
-                          </Popover> */}
-                        </div>
+                        <TooltipBadge
+                          color="danger"
+                          id={row}
+                          situation="Verificar"
+                          name={rowNames[row].name}
+                          obs={rowNames[row].obs}
+                          expected={rowNames[row].expected}
+                        />
                       )
                       : (
-                        <div>
-                          <Button id={row} color="success" type="button">OK</Button>
-                          {/* <Popover placement="right" isOpen={true} target={row} toggle={""}>
-                            <PopoverHeader>Title</PopoverHeader>
-                            <PopoverBody>Text</PopoverBody>
-                          </Popover> */}
-                        </div>
+                        <TooltipBadge
+                          color="success"
+                          id={row}
+                          situation="OK"
+                          name={rowNames[row].name}
+                          obs={rowNames[row].obs}
+                          expected={rowNames[row].expected}
+                        />
                       )
                     }
                   </td>
---
+++ src/components/Tooltips/TooltipBadge.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Button, Tooltip, PopoverHeader, PopoverBody, Badge } from 'reactstrap';
+
+class PopoverItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      tooltipOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      tooltipOpen: !this.state.tooltipOpen
+    });
+  }
+
+   render() {
+    return (
+      <span>
+        <Badge color={this.props.color} id={'Tooltip-' + this.props.id}>
+          {this.props.situation}
+        </Badge>
+        <Tooltip autohide={false} placement="right" isOpen={this.state.tooltipOpen} target={'Tooltip-' + this.props.id} toggle={this.toggle}>
+          <strong>{this.props.name}</strong>
+          <br/>
+          <br/>
+          <strong>Observações:</strong>
+          <br/>
+          <p style={{ "text-align": "justify" }}>{this.props.obs}</p>
+          <strong>Faixa de normalidade:</strong>
+          <br/>
+          {this.props.expected}
+        </Tooltip>
+      </span>
+    );
+  }
+}
+
+export default PopoverItem;
