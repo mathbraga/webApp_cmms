604d58f3d3bc58399355057a592fcaba270c5e1a
Pedro Henrique Serafim <serafaobr@gmail.com>
2019-04-12 10:47:54 -0300

    Fix bugs from merge

--- src/components/Reports/ReportProblems.jsx
+++ src/components/Reports/ReportProblems.jsx
@@ -32,12 +32,12 @@ class ReportProblems extends Component {
       dateWithFourDigits(this.props.finalDate)
     );
 
-
     let rowNames = {
       dcp: {
         name: "Demanda contratada - Ponta",
         unit: "kW",
-        obs: "Maior que zero somente na modalidade tarifária horária Azul. Igual a zero nos outros casos.",
+        obs:
+          "Maior que zero somente na modalidade tarifária horária Azul. Igual a zero nos outros casos.",
         expected: "≥ 0 kW"
       },
       dcf: {
@@ -55,62 +55,72 @@ class ReportProblems extends Component {
       dmf: {
         name: "Demanda medida - Fora de ponta",
         unit: "kW",
-        obs: "Maior demanda de potência ativa registrada no período - Fora de ponta",
+        obs:
+          "Maior demanda de potência ativa registrada no período - Fora de ponta",
         expected: "≤ Demanda contratada (Fora de ponta)"
       },
       dfp: {
         name: "Demanda faturada - Ponta",
         unit: "kW",
-        obs: "Demanda considerada no faturamento (maior valor entre medida e contratada) - Ponta",
+        obs:
+          "Demanda considerada no faturamento (maior valor entre medida e contratada) - Ponta",
         expected: "≤ Demanda contratada (Ponta)"
       },
       dff: {
         name: "Demanda faturada - Fora de ponta",
         unit: "kW",
-        obs: "Demanda considerada no faturamento (maior valor entre medida e contratada) - Fora de ponta",
+        obs:
+          "Demanda considerada no faturamento (maior valor entre medida e contratada) - Fora de ponta",
         expected: "≤ Demanda contratada (Fora de ponta)"
       },
-    
+
       vudp: {
         name: "Custo da ultrapassagem de demanda - Ponta",
         unit: "R$",
-        obs: "Valor adicional em caso de demanda medida superior à demanda contratada",
+        obs:
+          "Valor adicional em caso de demanda medida superior à demanda contratada",
         expected: "= R$ 0,00"
       },
-    
+
       vudf: {
         name: "Custo da ultrapassagem de demanda - Fora de ponta",
         unit: "R$",
-        obs: "Valor adicional em caso de demanda medida superior à demanda contratada",
+        obs:
+          "Valor adicional em caso de demanda medida superior à demanda contratada",
         expected: "= R$ 0,00"
       },
-    
+
       verexp: {
         name: "Custo do EREX - Ponta",
         unit: "R$",
-        obs: "Valor adicional em caso de excedentes de energia reativa (fator de potência inferior a 0,92)",
-        expected: "= R$ 0,00" },
-    
+        obs:
+          "Valor adicional em caso de excedentes de energia reativa (fator de potência inferior a 0,92)",
+        expected: "= R$ 0,00"
+      },
+
       verexf: {
         name: "Custo do EREX - Fora de ponta",
         unit: "R$",
-        obs: "Valor adicional em caso de excedentes de energia reativa (fator de potência inferior a 0,92)",
-        expected: "= R$ 0,00" },
-    
+        obs:
+          "Valor adicional em caso de excedentes de energia reativa (fator de potência inferior a 0,92)",
+        expected: "= R$ 0,00"
+      },
+
       jma: {
         name: "Multas, juros e atualização monetária",
         unit: "R$",
-        obs: "Valores adicionais decorrentes do atraso no pagamento de faturas anteriores",
+        obs:
+          "Valores adicionais decorrentes do atraso no pagamento de faturas anteriores",
         expected: "= R$ 0,00"
       },
       desc: {
         name: "Descontos e compensações",
         unit: "R$",
-        obs: "Total de descontos e compensações devido a baixos indicadores de qualidade do serviço, conforme normas da ANEEL, ou correções de valores cobrados indevidamente em faturas anteriores",
+        obs:
+          "Total de descontos e compensações devido a baixos indicadores de qualidade do serviço, conforme normas da ANEEL, ou correções de valores cobrados indevidamente em faturas anteriores",
         expected: "= R$ 0,00"
       }
     };
-  
 
     return (
       <Modal
@@ -189,52 +199,6 @@ class ReportProblems extends Component {
               {Object.keys(rowNames).map(row => (
                 <tr>
                   <th scope="row">{rowNames[row].name}</th>
-                  
-                  {rowNames[row].unit === "R$"
-                    ? (
-                      <td style={{ "text-align": "center" }}>
-                        {"R$ "}
-                        {this.props.problems && formatNumber(this.props.problems[row].value)}
-                      </td>
-                    )
-                    : (
-                      <td style={{ "text-align": "center" }}>
-                        {this.props.problems && formatNumber(this.props.problems[row].value, 0)}
-                        {" " + rowNames[row].unit}
-                      </td>
-                    )
-                  }
-                  
-                  
-                  
-                  <td style={{ "text-align": "center" }}>
-                    {this.props.problems && this.props.problems[row].problem
-                      ? (
-                          <BadgeWithTooltips
-                            color="danger"
-                            id={row}
-                            situation="Verificar"
-                            name={rowNames[row].name}
-                            obs={rowNames[row].obs}
-                            expected={rowNames[row].expected}
-                            problem={this.props.problems[row]}
-                            meters={this.props.meters}
-                          />
-                      )
-                      : (
-                        <BadgeWithTooltips
-                          color="success"
-                          id={row}
-                          situation="OK"
-                          name={rowNames[row].name}
-                          obs={rowNames[row].obs}
-                          expected={rowNames[row].expected}
-                          problem={this.props.problems[row]}
-                          meters={this.props.meters}
-                        />
-                      )
-                    }
-                  </td>
 
                   {rowNames[row].unit === "R$" ? (
                     <td style={{ "text-align": "center" }}>
@@ -259,6 +223,8 @@ class ReportProblems extends Component {
                         name={rowNames[row].name}
                         obs={rowNames[row].obs}
                         expected={rowNames[row].expected}
+                        problem={this.props.problems[row]}
+                        meters={this.props.meters}
                       />
                     ) : (
                       <BadgeWithTooltips
@@ -268,6 +234,8 @@ class ReportProblems extends Component {
                         name={rowNames[row].name}
                         obs={rowNames[row].obs}
                         expected={rowNames[row].expected}
+                        problem={this.props.problems[row]}
+                        meters={this.props.meters}
                       />
                     )}
                   </td>
