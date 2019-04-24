10399cb98e2ccd5c5128761fb194395f842b7ce6
hzlopes <hzlopes@senado.leg.br>
2019-04-24 16:39:49 -0300

    Add comments and pseudocode of ideas for handling with the token and AWS credentials

--- src/utils/authentication/loginCognito.js
+++ src/utils/authentication/loginCognito.js
@@ -26,10 +26,52 @@ export default function loginCognito(email, password){
 
   cognitoUser.authenticateUser(authenticationDetails, {
     onSuccess: function (result) {
+        console.log(result); // result = CognioUserSession
         var accessToken = result.getAccessToken().getJwtToken();
         console.log("Access token:");
         console.log(accessToken);
-        alert("Login efetuado com sucesso.\nUsuário logado: " + email);
+        alert("Login efetuado com sucesso.\n\nUsuário logado: " + email);
+        window.sessionStorage.setItem("accessToken", accessToken);
+        console.log(window);
+
+
+
+        // 1) adicionar funções do package jose (usar token com segurança)
+
+
+        // 2) inicializar uma credential do AWS (identity pool com role que tenha uma policy adequada ao usuário logado)
+        // segue abaixo um exemplo de como fazer isso:
+
+
+
+
+        // //POTENTIAL: Region needs to be set if not already set previously elsewhere.
+        // AWS.config.region = '<region>';
+
+        // AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        //     IdentityPoolId : '...', // your identity pool id here
+        //     Logins : {
+        //         // Change the key below according to the specific region your user pool is in.
+        //         'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>' : result.getIdToken().getJwtToken()
+        //     }
+        // });
+
+        // //refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
+        // AWS.config.credentials.refresh((error) => {
+        //     if (error) {
+        //          console.error(error);
+        //     } else {
+        //          // Instantiate aws sdk service objects now that the credentials have been updated.
+        //          // example: var s3 = new AWS.S3();
+        //          console.log('Successfully logged!');
+        //     }
+        // });
+
+
+
+
+
+
     },
     onFailure: function(err) {
         console.log("Login falhou.");
