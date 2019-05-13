cccc615c19c3b6855c9fa3d3c1fd1fa4c196bd50
hzlopes <hzlopes@senado.leg.br>
2019-05-13 15:46:27 -0300

    Add check for currentMonth and currentYear

--- src/utils/energy/checkSearchInputs.js
+++ src/utils/energy/checkSearchInputs.js
@@ -1,29 +1,41 @@
 export default function checkSearchInputs(initialDate, finalDate, oneMonth){
   
-  // Conditions both cases (one month or period) --> initialDate check
+  // Generate currentMonth and currentYear for comparison
+  let today = new Date();
+  let currentMonth = today.getMonth(); // January is 0 ... December is 11
+  let currentYear = today.getFullYear().toString();
+  if(currentMonth < 10){
+    currentMonth = "0" + currentMonth.toString();
+  } else {
+    currentMonth = currentMonth.toString();
+  }
+ 
+  // initialDate check
+  // These conditions are applied for both cases (one month or period)
   if(
-    initialDate.length < 7 ||           // Incomplete input
-    initialDate.slice(0, 2) > "12" ||   // Non-existent month
-    initialDate.slice(0, 2) === "00" || // Non-existent month
-    initialDate.slice(3) < "2017" ||    // Non-existent year in database
-    initialDate.slice(3) > "2019"       // Non-existent year in database
+    initialDate.length < 7 ||                                                       // Incomplete input
+    initialDate.slice(0, 2) > "12" ||                                               // Non-existent month
+    initialDate.slice(0, 2) === "00" ||                                             // Non-existent month
+    initialDate.slice(3) < "2017" ||                                                // Non-existent year in database
+    initialDate.slice(3) > currentYear ||                                           // Non-existent year in database
+    initialDate.slice(3) === currentYear && initialDate.slice(0, 2) > currentMonth  // Non-existent month in database
   ){
     return false;
   } else {
     
-    // Special conditions for period --> finalDate check
-    if (!oneMonth){
+    // finalDate check
+    // These conditions are applied only for period case
+    if(!oneMonth){
       if(
-      initialDate === finalDate ||      // Same dates
-      finalDate.length < 7 ||           // Incomplete input
-      finalDate.slice(0, 2) > "12" ||   // Non-existent month
-      finalDate.slice(0, 2) === "00" || // Non-existent month
-      finalDate.slice(3) < "2017" ||    // Non-existent year in database
-      finalDate.slice(3) > "2019" ||    // Non-existent year in database
-      // initialDate after finalDate (year check)
-      initialDate.slice(3) > finalDate.slice(3) ||
-      // initialDate after finalDate (same year, month check)
-      (initialDate.slice(3) === finalDate.slice(3) && initialDate.slice(0, 2) > finalDate.slice(0, 2))
+      initialDate === finalDate ||                                                                      // Same dates
+      finalDate.length < 7 ||                                                                           // Incomplete input
+      finalDate.slice(0, 2) > "12" ||                                                                   // Non-existent month
+      finalDate.slice(0, 2) === "00" ||                                                                 // Non-existent month
+      finalDate.slice(3) < "2017" ||                                                                    // Non-existent year in database
+      finalDate.slice(3) > currentYear ||                                                               // Non-existent year in database
+      finalDate.slice(3) === currentYear && finalDate.slice(0, 2) > currentMonth ||                     // Non-existent month in database
+      initialDate.slice(3) > finalDate.slice(3) ||                                                      // initialDate after finalDate (year check)
+      initialDate.slice(3) === finalDate.slice(3) && initialDate.slice(0, 2) > finalDate.slice(0, 2)  // initialDate after finalDate (same year, month check)
       ) {
         return false;
       }
