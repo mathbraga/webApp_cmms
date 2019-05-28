3f970b83833ad877cd13fa9355ae432de5baa020
hzlopes <hzlopes@senado.leg.br>
2019-05-28 12:48:30 -0300

    Remove Logout container

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ import MainPage from "./containers/MainPage";
 const ConsumptionMonitor = React.lazy(() => import("./containers/ConsumptionMonitor"));
 const Dashboard = React.lazy(() => import("./containers/Dashboard"));
 const Login = React.lazy(() => import("./containers/Authentication/Login"));
-const Logout = React.lazy(() => import("./containers/Authentication/Logout"));
 const SignUp = React.lazy(() => import("./containers/Authentication/SignUp"));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
