9e9d1a6c36dd4e2f35c46097112bdc8dc330af1e
hzlopes <hzlopes@gmail.com>
2019-06-01 17:35:00 -0300

    Add auto left margin in navbar (auth links to the right)

--- src/containers/MainPage/MainHeader.jsx
+++ src/containers/MainPage/MainHeader.jsx
@@ -39,15 +39,7 @@ class MainHeader extends Component {
           minimized={{ src: sygnet, width: 30, height: 30, alt: "Senado Logo" }}
         />
         <AppSidebarToggler className="d-md-down-none" display="lg" />
-
-        <Nav className="d-md-down-none" navbar>
-
-          {/* <NavItem className="px-3">
-            <Link to="/" className="nav-link">
-              Início
-            </Link>
-          </NavItem> */}
-
+        <Nav className="d-md-down-none ml-auto" navbar>
           {!this.props.session &&
             <React.Fragment>
               <NavItem className="px-3">
@@ -62,30 +54,22 @@ class MainHeader extends Component {
               </NavItem>
             </React.Fragment>
           }
-          
-          <AppHeaderDropdown direction="down">
-            
-            {this.props.session &&
+          {this.props.session &&
+            <AppHeaderDropdown direction="down">
               <React.Fragment>
-                <DropdownToggle nav>
+                <DropdownToggle nav className="px-3">
                   <i className="fa fa-user-circle"/>
                   {" " + this.props.session.idToken.payload.email}
                 </DropdownToggle>
-                
                 <DropdownMenu right style={{ right: 'auto' }}>
-                  <DropdownItem><i className="fa fa-user"></i>Perfil</DropdownItem>
-                  <DropdownItem><i className="fa fa-wrench"></i>Configurações</DropdownItem>
+                  {/* <DropdownItem><i className="fa fa-user"></i>Perfil</DropdownItem> */}
+                  {/* <DropdownItem><i className="fa fa-wrench"></i>Configurações</DropdownItem> */}
                   <DropdownItem onClick={this.handleLogout}><i className="fa fa-lock"></i> Logout</DropdownItem>
                 </DropdownMenu>
               </React.Fragment>
-            }
-            
-
-          </AppHeaderDropdown>
-
+            </AppHeaderDropdown>
+          }
         </Nav>
-
-        <Nav className="ml-auto" navbar />
       </React.Fragment>
     );
   }
