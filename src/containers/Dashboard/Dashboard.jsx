fdd97a15c3f2ead15adbcadb42ade1730d2c56ab
hzlopes <hzlopes@senado.leg.br>
2019-06-04 15:25:13 -0300

    Add rel=noopener in anchor tags

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -23,8 +23,11 @@ class Dashboard extends Component {
                     <div className="text-muted text-justify">
                       <p>
                         Este portal dá acesso ao{" "}
-                        <a href="https://en.wikipedia.org/wiki/Computerized_maintenance_management_system" target="_blank">
-                          sistema de gestão de manutenção
+                        <a
+                          href="https://en.wikipedia.org/wiki/Computerized_maintenance_management_system"
+                          target="_blank"
+                          rel="noopener"
+                          >sistema de gestão de manutenção
                         </a>
                         {" "}da SINFRA.
                       </p>
@@ -38,8 +41,11 @@ class Dashboard extends Component {
                         Novas funcionalidades serão adicionadas em breve.
                       </p>
                       <p>Para contribuições:{" "}
-                        <a href="https://github.com/Serafabr/cmms-web-app" target="_blank">
-                          repositório no GitHub{" "}
+                        <a
+                          href="https://github.com/Serafabr/cmms-web-app"
+                          target="_blank"
+                          rel="noopener"
+                        >repositório no GitHub{" "}
                           <i className="fa fa-github"></i>
                         </a>
                       </p>
