b1d0845cc2cb6bb63cef4df089821de552e80304
hzlopes <hzlopes@senado.leg.br>
2019-06-05 09:20:45 -0300

    Change text

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -12,7 +12,7 @@ class Dashboard extends Component {
         <div className="flex-row align-items-center">
           <Container>
             <Row className="justify-content-center">
-              <Col md="8">
+              <Col md="7">
                 <Card className="mx-4">
                   <CardBody className="p-4">
                     <h3>
@@ -22,35 +22,38 @@ class Dashboard extends Component {
                     <br/>
                     <div className="text-muted text-justify">
                       <p>
-                        Este portal dá acesso ao{" "}
+                        Este portal dá acesso ao sistema de gestão de manutenção da
+                        Secretaria de Infraestrutura do Senado Federal.
+                        {/* {" "}
                         <a
                           href="https://en.wikipedia.org/wiki/Computerized_maintenance_management_system"
                           target="_blank"
-                          rel="noopener noreferrer"
+                          rel="noopener noreferrer nofollow"
                           >sistema de gestão de manutenção
                         </a>
-                        {" "}da SINFRA.
+                        {" "}da SINFRA. */}
                       </p>
                       <p>
-                        Aqui serão disponibilizadas ferramentas computacionais para os colaboradores da Secretaria
-                        e divulgadas informações úteis para outros setores do Senado Federal.
+                        Aqui serão disponibilizadas ferramentas para os colaboradores desse setor
+                        e divulgadas informações úteis.
                       </p>
                       <p>
-                        A versão atual inclui a funcionalidade do "monitor de consumo",
-                        que permite pesquisar dados das faturas de água (CAESB) e energia elétrica (CEB).
-                        Novas funcionalidades serão adicionadas em breve.
+                        Neste momento está disponível o "monitor de consumo",
+                        que permite pesquisar dados das faturas de energia elétrica.
+                        Novas funcionalidades estão em desenvolvimento e serão adicionadas em breve.
                       </p>
-                      <p>Para contribuições:{" "}
+                      <p>
+                        Para reportar erros, sanar dúvidas ou dar sugestões,
+                        entre em contato com o SEPLAG no ramal 2339 ou envie um email para ls_seplag@senado.leg.br.
+                      </p>
+                      <p>Para contribuir ao código-fonte, acesse o{" "}
                         <a
                           href="https://github.com/Serafabr/cmms-web-app"
                           target="_blank"
-                          rel="noopener noreferrer"
-                        >repositório no GitHub{" "}
-                          <i className="fa fa-github"></i>
+                          rel="noopener noreferrer nofollow"
+                        >repositório no GitHub.
+                          {/*{" "}<i className="fa fa-github"></i> */}
                         </a>
-                      </p>
-                      <p>
-                        Dúvidas e sugestões: SEPLAG (ramal 2339)
                       </p>
                     </div>
                   </CardBody>
