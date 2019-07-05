d49886762c13639fdb0926effaa8d87a798599b7
hzlopes <hzlopes@senado.leg.br>
2019-07-05 11:30:40 -0300

    Remove LOCAL and REQUEST files

--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,10 @@ const Dashboard = React.lazy(() => import("./containers/Dashboard"));
 const Login = React.lazy(() => import("./containers/Authentication/Login"));
 const SignUp = React.lazy(() => import("./containers/Authentication/SignUp"));
 const Assets = React.lazy(() => import("./containers/Assets"));
-const WorkRequests = React.lazy(() => import("./containers/Maintenance/WorkRequests"));
-const NewWorkRequestForm = React.lazy(() => import("./containers/Maintenance/NewWorkRequestForm"));
 const WorkOrders = React.lazy(() => import("./containers/Maintenance/WorkOrders"));
 const NewWorkOrderForm = React.lazy(() => import("./containers/Maintenance/NewWorkOrderForm"));
 const WorkOrderView = React.lazy(() => import("./containers/Maintenance/WorkOrderView"));
 const AssetView = React.lazy(() => import("./containers/Maintenance/AssetView"));
-const LocalView = React.lazy(() => import("./containers/Maintenance/LocalView"));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
@@ -23,13 +20,10 @@ const routes = [
   { path: "/energia", name: "Energia elétrica", component: ConsumptionMonitor },
   { path: "/agua", name: "Água", component: ConsumptionMonitor },
   { path: "/ativos", exact: true, name: "Ativos", component: Assets },
-  { path: "/manutencao/solicitacoes", exact: true, name: "Solicitações", component: WorkRequests },
-  { path: "/manutencao/solicitacoes/nova", exact: true, name: "Nova solicitação", component: NewWorkRequestForm },
+  { path: "/ativos/view/:id", exact: false, name: "Ativo", component: AssetView },
   { path: "/manutencao/os", exact: true, name: "Ordens de serviços", component: WorkOrders },
   { path: "/manutencao/os/nova", exact: true, name: "Nova OS", component: NewWorkOrderForm },
   { path: "/manutencao/os/view/:id", exact: false, name: "OS", component: WorkOrderView },
-  { path: "/ativos/view/:id", exact: false, name: "Ativo", component: AssetView },
-  { path: "/local/view/:id", exact: false, name: "Local", component: LocalView }
 ];
 
 export default routes;
