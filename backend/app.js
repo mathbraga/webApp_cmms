e6bec860e4df223503a0f443fc89d4309795c6fa
hzlopes <hzlopes@senado.leg.br>
2019-08-12 12:43:05 -0300

    Replace connection string by connection object in app.js

--- backend/app.js
+++ backend/app.js
@@ -8,16 +8,28 @@ const { postgraphile } = require("postgraphile");
 // Middlewares
 // app.use(middleware);
 app.use(postgraphile(
-  process.env.DATABASE_URL || "postgres://postgres:123456@localhost:5432/cmms",
+  
+  // pgConfig (object)
+  {
+    host: '172.30.49.152',
+    port: 5432,
+    database: 'cmms',
+    user: 'postgres',
+    password: '123456',
+  },
+
+  // schemaName (string)
   "public",
+
+  // options (object)
   {
     // Check other options at:
     // https://www.graphile.org/postgraphile/usage-library/#api-postgraphilepgconfig-schemaname-options
     watchPg: true,
     enableCors: true,
-    exportJsonSchemaPath: "../schema.json",
-    exportGqlSchemaPath: "../schema.graphql",
-    sortExport: true,
+    // exportJsonSchemaPath: "../schema.json",
+    // exportGqlSchemaPath: "../schema.graphql",
+    // sortExport: true,
     graphqlRoute: "/",
     graphiql: true,
     graphiqlRoute: "/graphiql",
