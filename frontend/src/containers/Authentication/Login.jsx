50d601955a0efbb5ef7e780bf867e24a2148ec2e
hzlopes <hzlopes@senado.leg.br>
2019-09-10 09:33:28 -0300

    Change event.which to event.key (more explicit)

--- frontend/src/containers/Authentication/Login.jsx
+++ frontend/src/containers/Authentication/Login.jsx
@@ -37,7 +37,7 @@ class Login extends Component {
 
   handleEnterKeyDown = event => {
     event.persist();
-    if(event.which === 13){
+    if(event.key === "Enter"){
       this.handleSubmit(event);
     }
   }
