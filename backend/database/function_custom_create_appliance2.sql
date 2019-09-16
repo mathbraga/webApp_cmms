d7635373eaf7dc91c92e7fdbd968ff167777526d
hzlopes <hzlopes@senado.leg.br>
2019-09-16 07:45:21 -0300

    Add assets_departments to function, create cmms3 file (with contracts part of cmms new ideas)

--- backend/database/function_custom_create_appliance2.sql
+++ backend/database/function_custom_create_appliance2.sql
@@ -9,10 +9,24 @@ LANGUAGE plpgsql
 AS $$
 DECLARE	
   new_appliance_id text;
+  dept text;
 BEGIN
 
 INSERT INTO	appliances VALUES (input_values.*)
   returning asset_id into new_appliance_id;
+
+IF departments_array IS NOT NULL THEN
+  FOREACH	dept IN ARRAY departments_array::text[] LOOP
+    INSERT INTO	assets_departments (
+      asset_id,
+      department_id
+    ) VALUES (	
+      new_appliance_id,
+      dept
+    );
+  END LOOP;
+END IF;
+
 
 return new_appliance_id;
 
@@ -21,7 +35,7 @@ end; $$;
 begin;
 set local auth.data.person_id to 1;
 select custom_create_appliance2 (
-  ('sadfssdfdsdfs',
+  ('zzksfdkkzz',
   'ACAT-000-QDR-00308',
   'name',
   'description',
@@ -32,6 +46,6 @@ select custom_create_appliance2 (
   65465,
   'warranty',
   'CASF-000-000'),
-  ARRAY['SINFRA']
+  null
 );
 commit;
