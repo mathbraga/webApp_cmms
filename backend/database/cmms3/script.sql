b48fde327fcc987c22c542af90df491258a53acc
hzlopes <hzlopes@senado.leg.br>
2019-09-23 14:56:18 -0300

    Add more smart comments

--- backend/database/cmms3/script.sql
+++ backend/database/cmms3/script.sql
@@ -6996,6 +6996,13 @@ comment on table order_assets is E'@omit create,update,delete';
 comment on table order_messages is E'@omit create,update,delete';
 comment on table departments is E'@omit create,update,delete';
 comment on table contracts is E'@omit create,update,delete';
+comment on table assets is E'@omit all';
+comment on table contracts is E'@omit all';
+comment on table persons is E'@omit all';
+comment on table order_messages is E'@omit all';
+comment on constraint persons_pkey on persons is E'@omit';
+comment on constraint persons_email_key on persons is E'@omit';
+comment on constraint contracts_pkey on contracts is E'@omit';
 
 -- commit transaction
 commit;
