65b79add3ad79f86df5a38e390b3f3e07e94da78
hzlopes <hzlopes@senado.leg.br>
2019-09-23 14:07:24 -0300

    Modify_order

--- backend/database/cmms3/script.sql
+++ backend/database/cmms3/script.sql
@@ -694,10 +694,6 @@ begin
 
 end; $$;
 
-
-
-
-
 create or replace function modify_facility (
   facility_id text,
   facility_attributes facilities,
@@ -738,8 +734,63 @@ begin
 
 end; $$;
 
+create or replace function modify_order (
+  orderid integer,
+  order_attributes orders,
+  assets_array text[]
+)
+returns integer
+language plpgsql
+as $$
+begin
+  update orders as o
+    set (
+      status,
+      priority,
+      category,
+      parent,
+      contract,
+      completed,
+      request_text,
+      request_department,
+      request_person,
+      request_contact_name,
+      request_contact_phone,
+      request_contact_email,
+      request_title,
+      request_local,
+      date_limit,
+      date_start,
+      updated_at
+    ) = (
+      order_attributes.status,
+      order_attributes.priority,
+      order_attributes.category,
+      order_attributes.parent,
+      order_attributes.contract,
+      order_attributes.completed,
+      order_attributes.request_text,
+      order_attributes.request_department,
+      order_attributes.request_person,
+      order_attributes.request_contact_name,
+      order_attributes.request_contact_phone,
+      order_attributes.request_contact_email,
+      order_attributes.request_title,
+      order_attributes.request_local,
+      order_attributes.date_limit,
+      order_attributes.date_start,
+      now()
+    ) where o.order_id = orderid;
 
+  delete from order_assets as oa where oa.order_id = orderid;
 
+  if assets_array is not null then
+    insert into order_assets select orderid, unnest(assets_array);
+  end if;
+
+  return orderid;
+
+end; $$;
 
 -- create comments
 -- comment on type order_status_type is E'
--- backend/database/cmms3/test.sql
+++ backend/database/cmms3/test.sql
@@ -1,62 +1,91 @@
 begin;
 ----------------------------------------------------
-create or replace function modify_facility (
-  facility_id text,
-  facility_attributes facilities,
-  departments_array text[]
+create or replace function modify_order (
+  orderid integer,
+  order_attributes orders,
+  assets_array text[]
 )
-returns text
+returns integer
 language plpgsql
 as $$
 begin
-  update assets as a
+  update orders as o
     set (
-      parent,
-      place,
-      name,
-      description,
+      status,
+      priority,
       category,
-      latitude,
-      longitude,
-      area
+      parent,
+      contract,
+      completed,
+      request_text,
+      request_department,
+      request_person,
+      request_contact_name,
+      request_contact_phone,
+      request_contact_email,
+      request_title,
+      request_local,
+      date_limit,
+      date_start,
+      updated_at
     ) = (
-      facility_attributes.parent,
-      facility_attributes.place,
-      facility_attributes.name,
-      facility_attributes.description,
-      facility_attributes.category,
-      facility_attributes.latitude,
-      facility_attributes.longitude,
-      facility_attributes.area
-    ) where a.asset_id = facility_id;
+      order_attributes.status,
+      order_attributes.priority,
+      order_attributes.category,
+      order_attributes.parent,
+      order_attributes.contract,
+      order_attributes.completed,
+      order_attributes.request_text,
+      order_attributes.request_department,
+      order_attributes.request_person,
+      order_attributes.request_contact_name,
+      order_attributes.request_contact_phone,
+      order_attributes.request_contact_email,
+      order_attributes.request_title,
+      order_attributes.request_local,
+      order_attributes.date_limit,
+      order_attributes.date_start,
+      now()
+    ) where o.order_id = orderid;
 
-  delete from asset_departments where asset_id = facility_id;
+  delete from order_assets as oa where oa.order_id = orderid;
 
-  if departments_array is not null then
-    insert into asset_departments select facility_id, unnest(departments_array);
+  if assets_array is not null then
+    insert into order_assets select orderid, unnest(assets_array);
   end if;
 
-  return facility_id;
+  return orderid;
 
 end; $$;
 ----------------------------------------------------
 set local auth.data.person_id to 1;
-select modify_facility(
-  'CASF-000-000',
+select modify_order(
+  1,
   (
-    'CASF-000-000',
-    'BL15-000-000',
-    'BL15-000-000',
-    'Novo nome',
-    'nova descr',
-    'F',
-    null,
-    null,
-    9999999
+  1,
+  'FIL',
+  'URG',
+  'ARC',
+  null,
+  null,
+  99,
+  'request_text',
+  'SADCON',
+  'request_person',
+  'request_contact_name',
+  'request_contact_phone',
+  'request_contact_email',
+  'request_title',
+  'request_local',
+  '2019-01-01',
+  '2019-01-01',
+  '2018-09-09',
+  '2019-09-09',
+  '2019-09-09'
   ),
-  ARRAY['SINFRA', 'SADCON']
+  ARRAY['CASF-000-000', 'BL14-MEZ-046', 'AX02-AFM-000']
 );
-select * from facilities where asset_id = 'CASF-000-000';
+select * from orders where order_id = 1;
 ----------------------------------------------------
 rollback;
-select * from facilities where asset_id = 'CASF-000-000';
+select * from orders where order_id = 1;
