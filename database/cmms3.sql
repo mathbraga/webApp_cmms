29846598bcbff9d48b02bc6df910ce868c4cc4b3
hzlopes <hzlopes@senado.leg.br>
2019-09-25 14:21:26 -0300

    Add comments to omit new tables from graphql schema

--- database/cmms3.sql
+++ database/cmms3.sql
@@ -6368,18 +6368,22 @@ create policy auth_policy on order_messages for all to auth
 -- create smart comments
 comment on function authenticate is E'@omit execute';
 comment on table assets is E'@omit create,update,delete';
-comment on view appliances is E'@omit create,update,delete';
-comment on view facilities is E'@omit create,update,delete';
+comment on table contracts is E'@omit all,create,update,delete';
+comment on table departments is E'@omit create,update,delete';
+comment on table persons is E'@omit all,create,update,delete';
 comment on table orders is E'@omit create,update,delete';
 comment on table asset_departments is E'@omit create,update,delete';
 comment on table order_assets is E'@omit create,update,delete';
 comment on table order_messages is E'@omit all,create,update,delete';
-comment on table departments is E'@omit create,update,delete';
-comment on table contracts is E'@omit all,create,update,delete';
-comment on table persons is E'@omit all,create,update,delete';
+comment on table specs is E'@omit all,create,update,delete';
+comment on table supplies is E'@omit read,all,many,create,update,delete';
+comment on table order_supplies is E'@omit all,create,update,delete';
+comment on view appliances is E'@omit create,update,delete';
+comment on view facilities is E'@omit create,update,delete';
 comment on constraint persons_pkey on persons is E'@omit';
 comment on constraint persons_email_key on persons is E'@omit';
 comment on constraint contracts_pkey on contracts is E'@omit';
+comment on constraint specs_pkey on specs is E'@omit';
 
 -- commit transaction
 commit;
