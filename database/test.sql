1b0aa5dd7dd6859023d7893336fbb79cdd55be01
hzlopes <hzlopes@senado.leg.br>
2019-09-27 14:42:30 -0300

    Fix balances view definition

--- database/test.sql
+++ database/test.sql
@@ -11,8 +11,8 @@ create view balances as
         sum(os.qty) as blocked
           from orders as o
           inner join order_supplies as os using (order_id)
-        where o.status <> 'CON'
-        group by os.contract_id, os.supply_id
+      where o.status <> 'CON'
+      group by os.contract_id, os.supply_id
     ),
     finished as (
       select
@@ -21,22 +21,22 @@ create view balances as
         sum(os.qty) as consumed
           from orders as o
           inner join order_supplies as os using (order_id)
-        where o.status = 'CON'
-        group by os.contract_id, os.supply_id
+      where o.status = 'CON'
+      group by os.contract_id, os.supply_id
     ),
     both_cases as (
       select s.contract_id,
              s.supply_id,
              s.qty_available as qty_initial,
-             coalesce(unfinished.blocked, 0) as blocked,
-             coalesce(finished.consumed, 0) as consumed
-        from supplies as s,
-             inner join unfinished using (contract_id, supply_id)
-             full outer join finished using (contract_id, supply_id)
-        group by contract_id, supply_id;
+             coalesce(sum(blocked), 0) as blocked,
+             coalesce(sum(consumed), 0) as consumed
+        from supplies as s
+        inner join unfinished using (contract_id, supply_id)
+        full outer join finished using (contract_id, supply_id)
+      group by s.contract_id, s.supply_id
     )
     select *,
-           qty_initial - blocked - consumed
+          qty_initial - blocked - consumed as available
       from both_cases;
 
 select * from balances;
