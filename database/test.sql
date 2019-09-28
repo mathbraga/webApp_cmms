a560ff234e339fe9aab58c81d90740a8a4648d19
hzlopes <hzlopes@gmail.com>
2019-09-28 16:17:20 -0300

    Fix get_asset_history

--- database/test.sql
+++ database/test.sql
@@ -1,32 +1,49 @@
 begin;
 set local auth.data.person_id to 1;
-create or replace function get_children_orders (
-  in parent_prefix text,
-  out child_id text,
-  out order_ids_list integer[],
-  out order_titles_list text[],
-  out order_status_list text[]
-)
-language sql
-strict
-immutable
-as $$
-  with
-    children as (
-      select asset_id
-        from assets
-        where starts_with(asset_id, parent_prefix)
-    )
-    select asset_id as child_id
-           array_agg(order_id) as order_ids_list,
-           array_agg(request_title) as order_titles_list,
-           array_agg(status) as order_status_list
-      from children
-      inner join order_assets using (asset_id)
-      inner join orders using (order_id)
-    group by child_id;
-$$;
-select * from get_children_orders('BL14');
+-- select modify_appliance(
+--   (
+--     'CIVL-BM-0001',
+--     'ELET-CA-0003',
+--     'CASF-000-000',
+--     'hahhah',
+--     'hehehehhehehwhwhwhhw',
+--     'A',
+--     null,
+--     null,
+--     null,
+--     null,
+--     null
+--   ),
+--   null
+-- );
+
+-- create or replace function get_children_orders (
+--   in parent_prefix text,
+--   out child_id text,
+--   out order_ids_list integer[],
+--   out order_titles_list text[],
+--   out order_status_list order_status_type[]
+-- )
+-- language sql
+-- strict
+-- immutable
+-- as $$
+--   with
+--     children as (
+--       select asset_id
+--         from assets
+--         where starts_with(asset_id, parent_prefix)
+--     )
+--     select asset_id as child_id,
+--            array_agg(order_id) as order_ids_list,
+--            array_agg(request_title) as order_titles_list,
+--            array_agg(status) as order_status_list
+--       from children
+--       inner join order_assets using (asset_id)
+--       inner join orders using (order_id)
+--     group by child_id;
+-- $$;
+-- select * from get_children_orders('BL14');
 
 
 
@@ -37,17 +54,16 @@ create or replace function get_asset_history (
   out created_at timestamptz,
   out asset_json jsonb
 )
+returns setof record
 language sql
 stable
 as $$
-  with target_asset as (
-    select '{"asset_id":"' || asset_id || '"}'::jsonb
-  )
-  select p.fullname,
+  select p.full_name,
          l.created_at,
          l.new_row as asset_json
     from private.logs as l
     inner join persons as p using (person_id)
-  where tablename = 'assets' and new_row ? target_asset;
+  where l.tablename = 'assets' and l.new_row @> ('{"asset_id": "' || asset_id || '"}')::jsonb;
 $$;
+select * from get_asset_history('CIVL-BM-0001');
 rollback;
