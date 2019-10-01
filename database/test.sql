98d8eb34fd80d4ccb02a49dfb6725a0c7b87750f
hzlopes <hzlopes@senado.leg.br>
2019-10-01 14:58:14 -0300

    Prepare sql commands for more tables in cmms db

--- database/test.sql
+++ database/test.sql
@@ -1,44 +1,14 @@
 begin;
-drop view if exists balances;
-create view balances as
-  with
-    unfinished as (
-      select
-        os.contract_id,
-        os.supply_id,
-        sum(os.qty) as blocked
-          from orders as o
-          inner join order_supplies as os using (order_id)
-      where o.status <> 'CON'
-      group by os.contract_id, os.supply_id
-    ),
-    finished as (
-      select
-        os.contract_id,
-        os.supply_id,
-        sum(os.qty) as consumed
-          from orders as o
-          inner join order_supplies as os using (order_id)
-      where o.status = 'CON'
-      group by os.contract_id, os.supply_id
-    ),
-    both_cases as (
-      select contract_id,
-             supply_id,
-             sum(coalesce(blocked, 0)) as blocked,
-             sum(coalesce(consumed, 0)) as consumed
-        from unfinished as u
-        full outer join finished as f using (contract_id, supply_id)
-      group by contract_id, supply_id
-    )
-    select s.contract_id,
-           s.supply_id,
-           s.qty_initial,
-           bc.blocked,
-           bc.consumed,
-           s.qty_initial - bc.blocked - bc.consumed as available
-      from both_cases as bc
-      inner join supplies as s using (contract_id, supply_id);
 
-select * from balances;
+drop function if exists get_delayed_orders;
+
+create or replace function get_delayed_orders ()
+returns setof orders
+language sql
+as $$
+  select * from orders where date_limit < now();
+$$;
+
+select * from get_delayed_orders();
+
 rollback;
