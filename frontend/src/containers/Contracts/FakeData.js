475da0a0b94bac5badca94e0ea0acef6e8b2f32e
Pedro Henrique Serafim <serafaobr@gmail.com>
2019-10-08 11:55:14 -0300

    Create fake data for contracts and materials

---
+++ frontend/src/containers/Contracts/FakeData.js
@@ -0,0 +1,29 @@
+const contracts = [
+  { id: "CT01102019", title: "Manutenção do sistema elétrico do Senado Federal", company: "RCS Tecnologia", status: "Vigente", finalDate: "10/10/2020", url: "https://www12.senado.leg.br/hpsenado" },
+  { id: "CT00972019", title: "Manutenção do sistema de impermeabilização do Senado Federal", company: "PREFAC", status: "Vigente", finalDate: "12/08/2021", url: "https://www12.senado.leg.br/hpsenado" },
+  { id: "CT00212019", title: "Avaliação da qualidade do ar do Senado Federal", company: "Ambientalis", status: "Vigente", finalDate: "20/05/2024", url: "https://www12.senado.leg.br/hpsenado" },
+  { id: "CT00072019", title: "Serviços de engenharia civil", company: "JB Construtora", status: "Vigente", finalDate: "15/01/2021", url: "https://www12.senado.leg.br/hpsenado" },
+  { id: "CT00222019", title: "Manutenção do sistema hidrossanitário do Senado Federal", company: "RCS Tecnologia", status: "Vigente", finalDate: "10/08/2020", url: "https://www12.senado.leg.br/hpsenado" },
+  { id: "CT00552019", title: "Manutenção do sistema de refrigeração do Senado Federal", company: "Entherm", status: "Vigente", finalDate: "12/08/2021", url: "https://www12.senado.leg.br/hpsenado" },
+  { id: "CT00972019", title: "Fornecimento de material para construção civil.", company: "CETRO", status: "Vigente", finalDate: "02/05/2020", url: "https://www12.senado.leg.br/hpsenado" },
+  { id: "CT00952019", title: "Fornecimento de cimento, areia e brita", company: "Contruções SA", status: "Vigente", finalDate: "12/08/2021", url: "https://www12.senado.leg.br/hpsenado" },
+  { id: "CT00412019", title: "Manutenção do sistema de nobreaks", company: "Engmil", status: "Vigente", finalDate: "12/08/2021", url: "https://www12.senado.leg.br/hpsenado" },
+];
+
+const materials = [
+  { id: "SF-0001-01", nome: "Tomada", category: "Sistema elétrico", subcategory: "Tomadas e Interruptores", qtd: "145", unit: "un" },
+  { id: "SF-0002-01", nome: "Interruptor", category: "Sistema elétrico", subcategory: "Tomadas e Interruptores", qtd: "155", unit: "un" },
+  { id: "SF-0003-01", nome: "Lâmpada", category: "Sistema elétrico", subcategory: "Iluminação", qtd: "14", unit: "un" },
+  { id: "SF-0004-01", nome: "Lustre", category: "Sistema elétrico", subcategory: "Iluminação", qtd: "4", unit: "un" },
+  { id: "SF-0005-01", nome: "Reator", category: "Sistema elétrico", subcategory: "Iluminação", qtd: "56", unit: "un" },
+  { id: "SF-0006-01", nome: "Ar-condicionado Split", category: "Sistema refrigeração", subcategory: "Split", qtd: "1", unit: "un" },
+  { id: "SF-0007-01", nome: "Registro para tubulação de ar-condicionado", category: "Sistema refrigeração", subcategory: "Split", qtd: "1", unit: "un" },
+  { id: "SF-0008-01", nome: "Vidro temperado", category: "Manutenção Civil", subcategory: "Vidros", qtd: "12", unit: "m²" },
+  { id: "SF-0009-01", nome: "Vidro blindado", category: "Manutenção Civil", subcategory: "Vidros", qtd: "122", unit: "m²" },
+  { id: "SF-0010-01", nome: "Vidro 20 mm", category: "Manutenção Civil", subcategory: "Vidros", qtd: "120", unit: "m²" },
+  { id: "SF-0011-01", nome: "Areia", category: "Manutenção Civil", subcategory: "Material de Construção", qtd: "120", unit: "Kg" },
+  { id: "SF-0012-01", nome: "Brita", category: "Manutenção Civil", subcategory: "Material de Construção", qtd: "120", unit: "Kg" },
+  { id: "SF-0013-01", nome: "Cimento", category: "Manutenção Civil", subcategory: "Material de Construção", qtd: "120", unit: "Kg" },
+  { id: "SF-0014-01", nome: "Pedra", category: "Manutenção Civil", subcategory: "Material de Construção", qtd: "120", unit: "Kg" },
+
+];
