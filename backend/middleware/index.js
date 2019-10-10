4374bd966ad952e24c40e1a4f4ab4da58d958cda
hzlopes <hzlopes@senado.leg.br>
2019-10-10 16:04:28 -0300

    Prepare testing code with nodemailer

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -1,4 +1,47 @@
+const nodemailer = require('nodemailer');
+
+async function email(hostname) {
+  
+  if(hostname === '172.30.49.152'){
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    // let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+          user: 'sophie.crist@ethereal.email',
+          pass: 'r71qsfREgH2hKCfP2V'
+      }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: 'sophie.crist@ethereal.email', // sender address
+      to: 'sophie.crist@ethereal.email', // list of receivers
+      subject: 'Hello ✔', // Subject line
+      text: 'Hello world?', // plain text body
+      html: '<b>Hello world?</b>' // html body
+    });
+
+    console.log('Message sent: %s', info.messageId);
+
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+  } else {
+    console.log('not hz.')
+  }
+}
+
+
+
 function middleware(req, res, next){
+
+  email(req.hostname)
+    .catch(console.error);
 
   const date = new Date();
 
