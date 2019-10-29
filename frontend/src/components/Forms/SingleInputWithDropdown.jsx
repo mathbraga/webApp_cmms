af0df3678b565e3a9de54c9048235f437b1cc5c4
Pedro Henrique Serafim <serafaobr@gmail.com>
2019-10-29 18:27:14 -0300

    Basic layout to assemble your filter on ModalCreateFilter

--- frontend/src/components/Forms/SingleInputWithDropdown.jsx
+++ frontend/src/components/Forms/SingleInputWithDropdown.jsx
@@ -113,11 +113,11 @@ class SingleInputWithDropDown extends Component {
     const newValueId = filteredList[prevState.hoveredItem].id;
 
     const renderedInput = this.props.label === 'Status' ?
-      newValue : (this.props.label === 'Prioridade' ? 
+      newValue : (this.props.label === 'Prioridade' ?
         newValue : (this.props.label === 'Categoria' ?
           newValue : (this.props.label === 'OS pai' ?
             newValue : newValue + " " + `(${newValueId})`)));
-    
+
     this.props.update(newValueId);
     return {
       chosenValue: newValue,
@@ -167,7 +167,7 @@ class SingleInputWithDropDown extends Component {
   }
 
   render() {
-    const { label, placeholder, listDropdown } = this.props;
+    const { label, placeholder, listDropdown, withLabel = true } = this.props;
     const { inputValue, isDropdownOpen, hoveredItem } = this.state;
     const inputId = 'input-list-' + this.props.id;
     const containerId = 'list-container-' + this.props.id;
@@ -186,7 +186,7 @@ class SingleInputWithDropDown extends Component {
 
     return (
       <FormGroup className={'dropdown-container'}>
-        <Label htmlFor="input">{label}</Label>
+        {withLabel && <Label htmlFor="input">{label}</Label>}
         <Input
           type="text"
           autoComplete="off"
--- frontend/src/components/Modals/ModalCreateFilter.jsx
+++ frontend/src/components/Modals/ModalCreateFilter.jsx
@@ -12,7 +12,7 @@ import {
   Label,
   Input
 } from "reactstrap";
-import AssetCard from "../Cards/AssetCard";
+import SingleInputWithDropdown from "../Forms/SingleInputWithDropdown";
 
 import "./ModalCreateFilter.css";
 
@@ -40,19 +40,78 @@ class ModalCreateFilter extends Component {
             <Col md="4" xs="6" className="container-left">
               <Button
                 onClick={() => { }}
-                className="ghost-button" style={{ width: "auto", height: "38px", padding: "8px 40px" }}>Filtros</Button>
+                className="ghost-button" style={{ width: "auto", height: "38px", padding: "8px 40px" }}>Filtros Prontos</Button>
             </Col>
           </Row>
         </div>
         <ModalBody>
-          <Form>
-            <FormGroup row>
-              <Label for="filterName" sm={2}>Nome</Label>
-              <Col sm={10}>
-                <Input type="text" name="filterName" id="filterName" />
-              </Col>
-            </FormGroup>
-          </Form>
+          <FormGroup row>
+            <Label for="filterName" sm={2}>Nome</Label>
+            <Col sm={10}>
+              <Input type="text" name="filterName" id="filterName" />
+            </Col>
+          </FormGroup>
+          <div className='create-filter-container'>
+            <div className={'filter-container-title'}>
+              <div className="filter-title">
+                Monte seu Filtro
+              </div>
+            </div>
+            <div className="filter-container-body">
+              <FormGroup row>
+                <Label for="attribute" sm={3}>Atributo / Operação</Label>
+                <Col sm={9}>
+                  <SingleInputWithDropdown
+                    withLabel={false}
+                    listDropdown={[{ id: 1, text: 'A', subtext: 'A' }, { id: 2, text: 'A', subtext: 'A' }, { id: 3, text: 'A', subtext: 'A' }]}
+                    required
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col sm={4} style={{ margin: "auto 0" }}>
+                  <Input disabled type="text" name="attribute" id="attribute" />
+                </Col>
+                <Col sm={4} style={{ margin: "auto 0" }}>
+                  <Input type="select" name="attribute" id="attribute" >
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                    <option>D</option>
+                    <option>E</option>
+                    <option>F</option>
+                    <option>G</option>
+                    <option>H</option>
+                    <option>I</option>
+                    <option>J</option>
+                  </Input>
+                </Col>
+                <Col sm={4}>
+                  <Input type="select" name="attribute" id="attribute" multiple >
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                    <option>D</option>
+                    <option>E</option>
+                    <option>F</option>
+                    <option>G</option>
+                    <option>H</option>
+                    <option>I</option>
+                    <option>J</option>
+                  </Input>
+                </Col>
+              </FormGroup>
+              <Button color="primary">Adicionar</Button>
+              <Button color="danger" style={{ marginLeft: "10px" }}>Limpar</Button>
+            </div>
+          </div>
+          <div className='create-filter-container'>
+            <div className={'filter-container-title'}>
+              <div className="filter-title">
+                Resultado
+              </div>
+            </div>
+          </div>
         </ModalBody>
         <ModalFooter className={'filter-footer'}>
           Hey
