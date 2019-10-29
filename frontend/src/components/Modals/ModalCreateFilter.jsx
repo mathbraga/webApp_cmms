6e5ef5a468e0cbe7a1c334be671b9f83f0e95f0c
Pedro Henrique Serafim <serafaobr@gmail.com>
2019-10-29 16:45:20 -0300

    Rebuild layout of the header on ModalCreateFilter

--- frontend/src/components/Modals/ModalCreateFilter.jsx
+++ frontend/src/components/Modals/ModalCreateFilter.jsx
@@ -1,28 +1,61 @@
 import React, { Component } from "react";
 import {
   Row,
+  Col,
   Button,
   Modal,
   ModalHeader,
   ModalBody,
   ModalFooter,
+  Form,
+  FormGroup,
+  Label,
+  Input
 } from "reactstrap";
+import AssetCard from "../Cards/AssetCard";
+
+import "./ModalCreateFilter.css";
 
 class ModalCreateFilter extends Component {
 
   render() {
     const { toggle, modal } = this.props;
     return (
-      <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>
-          Criar Filtro
-        </ModalHeader>
+      <Modal
+        isOpen={modal}
+        toggle={toggle}
+        size={'lg'}
+        backdrop={'static'}
+      >
+        <div className='filter-header'>
+          <Row>
+            <Col md="8" xs="6">
+              <div className="widget-title dash-title text-truncate">
+                <h4>Cadastrar Filtro</h4>
+                <div className="dash-subtitle text-truncate">
+                  Formulário para criação de filtros
+                </div>
+              </div>
+            </Col>
+            <Col md="4" xs="6" className="container-left">
+              <Button
+                onClick={() => { }}
+                className="ghost-button" style={{ width: "auto", height: "38px", padding: "8px 40px" }}>Filtros</Button>
+            </Col>
+          </Row>
+        </div>
         <ModalBody>
-          <div>Hey</div>
+          <Form>
+            <FormGroup row>
+              <Label for="filterName" sm={2}>Nome</Label>
+              <Col sm={10}>
+                <Input type="text" name="filterName" id="filterName" />
+              </Col>
+            </FormGroup>
+          </Form>
         </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggle}>Aplicar</Button>{' '}
-          <Button color="secondary" onClick={toggle}>Cancelar</Button>
+        <ModalFooter className={'filter-footer'}>
+          Hey
         </ModalFooter>
       </Modal>
     );
