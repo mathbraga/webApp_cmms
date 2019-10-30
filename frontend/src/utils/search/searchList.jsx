c80c95fb11db2645808f35ef58e5c17f5b8a16bf
Matheus Braga <matheus_ob59@hotmail.com>
2019-10-30 14:44:15 -0300

    Improve search inside forms dropdowns.

--- frontend/src/utils/search/searchList.jsx
+++ frontend/src/utils/search/searchList.jsx
@@ -29,7 +29,12 @@ const ORDER_STATUS_TYPE = {
 function findItem(items, attributes, term){
   const result = items.filter((item) => {
     for(let i = 0; i < attributes.length; i++){
-      let value = attributes[i].split('.').reduce(function(p,prop) { return p[prop] }, item.node);
+      let value = [];
+      if(attributes[i] === 'subtext' || attributes[i] === 'text')
+        value = attributes[i].split('.').reduce(function(p,prop) { return p[prop] }, item);
+      else
+        value = attributes[i].split('.').reduce(function(p,prop) { return p[prop] }, item.node);
+
       switch (attributes[i]) {
         case 'orderId':
           value = String(value).padStart(3, "0");
@@ -58,7 +63,12 @@ export default function searchList(itemsList, attributes, searchTerm){
   if(searchTermLower.length === 1){
     filteredItems = itemsList.filter((item) => {
       for(let i = 0; i < attributes.length; i++){
-        let value = attributes[i].split('.').reduce(function(p,prop) { return p[prop] }, item.node);
+        let value = [];
+        if(attributes[i] === 'subtext' || attributes[i] === 'text')
+          value = attributes[i].split('.').reduce(function(p,prop) { return p[prop] }, item);
+        else
+          value = attributes[i].split('.').reduce(function(p,prop) { return p[prop] }, item.node);
+          
         switch (attributes[i]) {
           case 'orderId':
             value = String(value).padStart(3, "0");
@@ -70,9 +80,8 @@ export default function searchList(itemsList, attributes, searchTerm){
             value = ORDER_STATUS_TYPE[value];
             break;
         }
-        if(String(value).toLowerCase().includes(searchTermLower[0])){
+        if(String(value).toLowerCase().includes(searchTermLower[0]))
           return String(value).toLowerCase().includes(searchTermLower[0]);
-        }
       }
     })
   }else if(searchTermLower.length > 1){
