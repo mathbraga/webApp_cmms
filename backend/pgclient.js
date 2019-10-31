0f5836951714db5d2ed285b48af88ac64a4c2488
hzlopes <hzlopes@senado.leg.br>
2019-10-31 16:14:44 -0300

    Change dbname in pgclient (fix authentication route)

--- backend/pgclient.js
+++ backend/pgclient.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 const client = new Client({
   user: process.env.DB_ADMIN,
   host: process.env.DB_HOST,
-  database: process.env.DB_DBNAME,
+  database: 'cmms6',
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
 });
