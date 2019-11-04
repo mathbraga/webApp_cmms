3a1fecb5c01aa3493498e279d5799909dea9e0d9
hzlopes <hzlopes@senado.leg.br>
2019-11-04 13:22:43 -0300

    Create view spec_orders

--- database/views.sql
+++ database/views.sql
@@ -69,3 +69,13 @@ create view balances as
       from both_cases as bc
       inner join supplies as s using (supply_id)
       inner join contracts as c using (contract_id);
+
+create view spec_orders as
+  select sp.spec_id,
+         o.order_id,
+         o.status,
+         o.title
+    from specs as sp
+    inner join supplies as su using (spec_id)
+    inner join order_supplies as os using (supply_id)
+    inner join orders as o using (order_id);
