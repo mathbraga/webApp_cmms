6229f9c03601a3fff2aee00fa2748133c1e43a93
Pedro Henrique Serafim <serafaobr@gmail.com>
2019-11-07 17:47:31 -0300

    Add filter to SpecsList

--- frontend/src/containers/Contracts/SpecsList.jsx
+++ frontend/src/containers/Contracts/SpecsList.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ModalCreateFilter from "../../components/Modals/ModalCreateFilter";
+import ApplyFilter from "../../components/Modals/ApplyFilter"
 import {
   Row,
   Col,
@@ -15,6 +17,7 @@ import { withRouter } from "react-router-dom";
 import "./List.css";
 
 import searchList from "../../utils/search/searchList";
+import filterList from "../../utils/filter/filter";
 // import { materials } from "./FakeData";
 
 const tableConfig = [
@@ -27,12 +30,50 @@ const tableConfig = [
 const searchItem = require("../../assets/icons/search_icon.png");
 
 const ENTRIES_PER_PAGE = 15;
+
 const attributes = [
   "specSf",
   "subcategory",
   "name",
   "category"
-]
+];
+
+const customFilters = [
+  {
+    id: "001",
+    name: "Sem Filtro",
+    author: "webSINFRA Software",
+    logic: [],
+  },
+  {
+    id: "002",
+    name: "Edifícios - Blocos de Apoio",
+    author: "webSINFRA Software",
+    logic: [
+      { attribute: 'name', type: 'att', verb: 'include', term: ["Bloco"] },
+    ],
+  },
+];
+
+const SPECS_CATEGORY = {
+  'Geral': 'Geral',
+  'Serviços de Apoio': 'Serviços de Apoio',
+  'Civil': 'Civil',
+  'Hidrossanitário': 'Hidrossanitário',
+  'Elétrica': 'Elétrica',
+  'Ar Condicionado': 'Ar Condicionado',
+  'Marcenaria e Serralheria': 'Marcenaria e Serralheria',
+  'Rede e Telefonia': 'Rede e Telefonia',
+  'Ferramentas e Equipamentos': 'Ferramentas e Equipamentos',
+}
+
+const filterAttributes = {
+  category: { name: 'Categoria', type: 'option', options: SPECS_CATEGORY },
+  name: { name: 'Material / Serviço', type: 'text' },
+  specSf: { name: 'Código', type: 'text' },
+  subcategory: { name: 'Subcategoria', type: 'text' },
+  version: { name: 'Versão', type: 'text' },
+};
 
 class SpecsList extends Component {
   constructor(props) {
@@ -40,12 +81,31 @@ class SpecsList extends Component {
     this.state = {
       pageCurrent: 1,
       goToPage: 1,
-      searchTerm: ""
+      searchTerm: "",
+      modalFilter: false,
+      modalApplyFilter: false,
+      filterLogic: [],
+      filterName: null,
+      filterSavedId: null,
     };
     this.setGoToPage = this.setGoToPage.bind(this);
     this.setCurrentPage = this.setCurrentPage.bind(this);
     this.handleChangeSearchTerm = this.handleChangeSearchTerm.bind(this);
     this.handleURLChange = this.handleURLChange.bind(this);
+    this.toggleCreate = this.toggleCreate.bind(this);
+    this.toggleApply = this.toggleApply.bind(this);
+  }
+
+  toggleCreate() {
+    this.setState((prevState) => ({
+      modalFilter: !prevState.modalFilter
+    }));
+  }
+
+  toggleApply() {
+    this.setState((prevState) => ({
+      modalApplyFilter: !prevState.modalApplyFilter
+    }));
   }
 
   handleChangeSearchTerm(event) {
@@ -66,14 +126,25 @@ class SpecsList extends Component {
     this.props.history.push('/gestao/servicos/novo');
   }
 
+  updateCurrentFilter = (filterLogic, filterName, filterId = null) => {
+    this.setState({
+      filterLogic,
+      filterName,
+      filterSavedId: filterId,
+      pageCurrent: 1,
+      goToPage: 1,
+    });
+  }
+
   render() {
     const { allItems } = this.props;
-    const { pageCurrent, goToPage, searchTerm } = this.state;
+    const { pageCurrent, goToPage, searchTerm, filterLogic } = this.state;
 
-    const filteredItems = searchList(allItems, attributes, searchTerm);
+    let filteredItems = filterLogic.length > 0 ? filterList(allItems, filterLogic) : allItems;
+    let searchedItems = searchList(filteredItems, attributes, searchTerm);
 
-    const pagesTotal = Math.floor(filteredItems.length / ENTRIES_PER_PAGE) + 1;
-    const showItems = filteredItems.slice((pageCurrent - 1) * ENTRIES_PER_PAGE, pageCurrent * ENTRIES_PER_PAGE);
+    const pagesTotal = Math.floor(searchedItems.length / ENTRIES_PER_PAGE) + 1;
+    const showItems = searchedItems.slice((pageCurrent - 1) * ENTRIES_PER_PAGE, pageCurrent * ENTRIES_PER_PAGE);
 
     const thead =
       <tr>
@@ -127,16 +198,16 @@ class SpecsList extends Component {
             <div className="search-filter" style={{ width: "30%" }}>
               <ol>
                 <li><span className="card-search-title">Filtro: </span></li>
-                <li><span className="card-search-title">Regras: </span></li>
+                <li><span className="card-search-title">Resultado: </span></li>
               </ol>
               <ol>
-                <li>Sem filtro</li>
-                <li>Mostrar todos itens</li>
+                <li>{(this.state.filterLogic.length === 0) ? "Sem filtro" : (this.state.filterName || "Filtro sem nome")}</li>
+                <li><span>Página com </span><b>{filteredItems.length.toString()}</b><span> itens</span></li>
               </ol>
             </div>
             <div className="search-buttons" style={{ width: "30%" }}>
-              <Button className="search-filter-button" color="success">Aplicar Filtro</Button>
-              <Button className="search-filter-button" color="primary">Criar Filtro</Button>
+              <Button className="search-filter-button" color="success" onClick={this.toggleApply}>Aplicar Filtro</Button>
+              <Button className="search-filter-button" color="primary" onClick={this.toggleCreate}>Criar Filtro</Button>
             </div>
           </div>
           <TableWithPages
@@ -147,6 +218,20 @@ class SpecsList extends Component {
             goToPage={goToPage}
             setCurrentPage={this.setCurrentPage}
             setGoToPage={this.setGoToPage}
+          />
+          <ModalCreateFilter
+            toggle={this.toggleCreate}
+            modal={this.state.modalFilter}
+            attributes={filterAttributes}
+            updateCurrentFilter={this.updateCurrentFilter}
+          />
+          <ApplyFilter
+            toggle={this.toggleApply}
+            modal={this.state.modalApplyFilter}
+            attributes={filterAttributes}
+            updateCurrentFilter={this.updateCurrentFilter}
+            customFilters={customFilters}
+            currentFilterId={this.state.filterSavedId}
           />
         </AssetCard >
       </div>
