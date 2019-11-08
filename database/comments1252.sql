3e48667f704f4e105d7c864e0ee30a044365cd04
Pedro Henrique Serafim <serafaobr@gmail.com>
2019-11-08 16:16:38 -0300

    Create custom filter to PersonsList

--- database/comments1252.sql
+++ database/comments1252.sql
@@ -1,35 +1,35 @@
 comment on type order_status_type is E'
-Significados dos possíveis valores do enum order_status_type:\n
+Significados dos possveis valores do enum order_status_type:\n
 CAN: Cancelada;\n 
 NEG: Negada;\n
 PEN: Pendente;\n
 SUS: Suspensa;\n
 FIL: Fila de espera;\n
-EXE: Execução;\n
-CON: Concluída.\n
+EXE: Execuo;\n
+CON: Concluda.\n
 ';
 
 comment on type order_category_type is E'
-Significados dos possíveis valores do enum order_category_type:\n
+Significados dos possveis valores do enum order_category_type:\n
 ARC: Ar-condicionado;\n
-ELE: Instalações elétricas;\n
+ELE: Instalaes eltricas;\n
 ELV: Elevadores;\n
-EST: Avaliação estrutural;\n
+EST: Avaliao estrutural;\n
 EXA: Exaustores;\n
 FOR: Reparo em forro;\n
 GRL: Geral;\n
-HID: Instalações hidrossanitárias;\n
-INF: Infiltração;\n
+HID: Instalaes hidrossanitrias;\n
+INF: Infiltrao;\n
 MAR: Marcenaria;\n
 PIS: Reparo em piso;\n
 REV: Revestimento;\n
 SER: Serralheria;\n
-VED: Vedação espacial;\n
-VID: Vidraçaria / Esquadria.\n
+VED: Vedao espacial;\n
+VID: Vidraaria / Esquadria.\n
 ';
 
 comment on type order_priority_type is E'
-Significados dos possíveis valores do enum order_priority_type:\n
+Significados dos possveis valores do enum order_priority_type:\n
 BAI: Baixa;\n
 NOR: Normal;\n
 ALT: Alta;\n
--- frontend/src/containers/Teams/PersonsList.jsx
+++ frontend/src/containers/Teams/PersonsList.jsx
@@ -45,14 +45,6 @@ const customFilters = [
     author: "webSINFRA Software",
     logic: [],
   },
-  {
-    id: "002",
-    name: "Edifícios - Blocos de Apoio",
-    author: "webSINFRA Software",
-    logic: [
-      { attribute: 'name', type: 'att', verb: 'include', term: ["Bloco"] },
-    ],
-  },
 ];
 
 const filterAttributes = {
