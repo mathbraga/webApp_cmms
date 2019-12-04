56fad5ef84caada381d892bbd5c7cb54a0e9e9d1
hzlopes <hzlopes@gmail.com>
2019-12-04 18:26:17 -0300

    Remove duplicity of passport objects

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,51 +1,6 @@
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const { pgClient } = require('../configs');
+const router = require('express').Router();
+const passport = require('../middlewares/passport');
 const paths = require('../paths');
-
-passport.use(new LocalStrategy(
-  {
-    usernameField: 'email',
-    passwordField: 'password'
-  },
-  async function(email, password, done){
-    // console.log('inside passport.use')
-    let data;
-    try {
-      data = await pgClient.query('select authenticate($1, $2)', [email, password]);
-      if (data.rows.length === 0) {
-        return done(null, false, {message: 'Incorrect'});
-      }
-    } catch (error) {
-      return done(error);
-    }
-    let userData = data.rows[0].authenticate;
-    // console.log(data.rows[0].authenticate);
-    return done(null, userData);
-  }
-));
-
-passport.serializeUser((userData, done) => {
-  console.log('\nSERIALIZATION\n')
-  console.log(userData)
-  done(null, userData);
-});
-
-passport.deserializeUser(async (userData, done) => {
-  console.log('\nDESERIALIZATION\n')
-  try {
-    let data = await pgClient.query('select person_id from persons where person_id = $1', [parseInt(userData.split('-')[0],10)]);
-    if (data.rows.length === 0){
-      return done(new Error('user not found'));
-    }
-    // console.log(JSON.stringify(data))
-    done(null, data.rows[0].person_id);
-  } catch (error) {
-    done(error);
-  }
-});
 
 router.post(paths.login,
   passport.authenticate('local'),
