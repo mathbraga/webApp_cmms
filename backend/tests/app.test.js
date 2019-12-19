2e89b4c12974564fb8642534fbe6c1231a43ab21
hzlopes <hzlopes@gmail.com>
2019-12-18 21:06:13 -0300

    Fix app.test.js

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -2,8 +2,17 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Test the root path', () => {
-    test('It should response the GET method', async () => {
-        const response = await request(app).get('/');
-        expect(response.statusCode).toBe(200);
-    });
-})
+  test('It should response the GET method', async () => {
+    try {
+      const response = await request(app)
+      .post('/db')
+      .set('Accept', 'application/json')
+      .send({
+        query: 'query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } } } fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } } fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue } fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } }'
+      });
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+});
