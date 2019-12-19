56d4ecba7b55f15587d6a40cd80c915e65c87e92
hzlopes <hzlopes@gmail.com>
2019-12-19 20:47:17 -0300

    New balances view version

--- database/test.sql
+++ database/test.sql
@@ -6,42 +6,32 @@ drop view balances cascade;
 create view balances as
   with
     finished as (
-      select task_id from tasks where task_status_id = 7
+      select task_id, supply_id, qty, task_status_id
+        from tasks
+        inner join task_supplies using (task_id)
+      where task_status_id = 7
     ),
     unfinished as (
-      select task_id from tasks where task_status_id <> 7
+      select task_id, supply_id, qty, task_status_id
+        from tasks
+        inner join task_supplies using (task_id)
+      where task_status_id <> 7
     ),
-    blocked as (
-      select ts.supply_id,
-             ts.qty as blocked
-        from task_supplies as ts
-        inner join unfinished as u using (task_id)
-    ),
-    consumed as (
-      select ts.supply_id,
-             ts.qty as consumed
-        from task_supplies as ts
-        inner join finished as f using (task_id)
-    ),
-    all_cases as (
-      select supply_id,
-             sum(coalesce(b.blocked, 0)) as blocked,
-             sum(coalesce(c.consumed, 0)) as consumed
-        from blocked as b
-        full outer join consumed as c using (supply_id)
-      group by supply_id
+    quantities as (
+      select s.supply_id,
+             s.qty as qty_initial,
+             sum(coalesce(f.qty, 0)) as qty_consumed,
+             sum(coalesce(u.qty, 0)) as qty_blocked
+        from finished as f
+        full outer join unfinished as u using (supply_id)
+        full outer join supplies as s using (supply_id)
+      group by s.supply_id, s.qty
     )
-    select s.supply_id,
-           s.qty as qty_initial,
-           coalesce(a.blocked, 0) as qty_blocked,
-           coalesce(a.consumed, 0) as qty_consumed,
-           s.qty - coalesce(a.blocked, 0) - coalesce(a.consumed, 0) as qty_available
-        from supplies as s
-        full outer join all_cases as a using (supply_id)
-    order by s.supply_id;
+    select *,
+           qty_initial - qty_blocked - qty_consumed as qty_available
+      from quantities;
       
 
-select * from balances;
-
+select * from balances order by supply_id;
 
 rollback;
