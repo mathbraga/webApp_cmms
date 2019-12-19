69235eabdacfdbe4723b128365d1ed1a8c725e4e
hzlopes <hzlopes@senado.leg.br>
2019-12-19 11:22:36 -0300

    Fix tests expects

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -1,33 +1,26 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('Test all app middlewares', () => {
+describe('Test all middlewares and routes', () => {
   
-  test('Authentication', async () => {
-    try {
-      const response = await request(app)
-      .post('/auth/login')
-      .send({
-        email: 'hzlopes@senado.leg.br',
-        password: '123456',
-      });
-      expect(response.statusCode).toBe(200);
-    } catch (err) {
-      console.log(err);
-    }
+  test('Passport (authentication)', async () => {
+    const response = await request(app)
+    .post('/auth/login')
+    .send({
+      email: 'hzlopes@senado.leg.br',
+      password: '123456',
+    })
+    .expect('Set-Cookie', /cmms:user/)
+    .expect(200);
   });
   
-  test('Postgraphile', async () => {
-    try {
-      const response = await request(app)
-      .post('/db')
-      .send({
-        query: 'query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } } } fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } } fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue } fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } }'
-      });
-      expect(response.statusCode).toBe(200);
-    } catch (err) {
-      console.log(err);
-    }
+  test('Postgraphile (introspection query)', async () => {
+    const response = await request(app)
+    .post('/db')
+    .send({
+      query: 'query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } } } fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } } fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue } fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } }'
+    })
+    .expect(200);
   });
 
 });
