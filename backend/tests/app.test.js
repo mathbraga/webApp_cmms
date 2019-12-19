d29ddf6ec0f6bc166314bb75ab014367cd3d63ea
hzlopes <hzlopes@senado.leg.br>
2019-12-19 10:48:05 -0300

    Add Authentication route test

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -1,18 +1,33 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('Test the root path', () => {
-  test('It should response the GET method', async () => {
+describe('Test all app middlewares', () => {
+  
+  test('Authentication', async () => {
     try {
       const response = await request(app)
-      .post('/db')
-      .set('Accept', 'application/json')
+      .post('/auth/login')
       .send({
-        query: 'query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } } } fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } } fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue } fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } }'
+        email: 'hzlopes@senado.leg.br',
+        password: '123456',
       });
-      console.log(response);
+      expect(response.statusCode).toBe(200);
     } catch (err) {
       console.log(err);
     }
   });
+  
+  test('Postgraphile', async () => {
+    try {
+      const response = await request(app)
+      .post('/db')
+      .send({
+        query: 'query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } } } fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } } fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue } fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } }'
+      });
+      expect(response.statusCode).toBe(200);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
 });
