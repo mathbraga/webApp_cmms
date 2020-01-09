c744b19a3b28286f00827049001440f21d4c78ff
hzlopes <hzlopes@gmail.com>
2020-01-09 19:12:12 -0300

    Standardize messages

--- database/functions-exception.sql
+++ database/functions-exception.sql
@@ -15,8 +15,8 @@ create or replace function get_exception_message (
         when exception_code = 2 then message = format('%s %s - Saldo insuficiente do suprimento.', header, exception_code);
         when exception_code = 3 then message = format('%s %s - Quantidade com decimais não permitida para o suprimento.', header, exception_code);
         when exception_code = 4 then message = format('%s %s - Contrato da tarefa não corresponde ao contrato dos suprimentos selecionados.', header, exception_code);
-        when exception_code = 5 then message = format('%s %s: Categoria de ativo inválida.', header, exception_code);
-        when exception_code = 6 then message = format('%s %s: Árvore de relações de ativos inválida.', header, exception_code);
+        when exception_code = 5 then message = format('%s %s - Categoria de ativo inválida.', header, exception_code);
+        when exception_code = 6 then message = format('%s %s - Árvore de relações de ativos inválida.', header, exception_code);
         else message = header;
       end case;
 
