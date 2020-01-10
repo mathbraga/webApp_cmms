1ceb8a4d33a4d1bb6a91022d1c91955bcc1e635e
hzlopes <hzlopes@senado.leg.br>
2020-01-10 09:24:52 -0300

    Reorganize tests folder

--- backend/tests/app/app.test.js
+++ backend/tests/app/app.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
-const app = require('../app');
-const { pgPool } = require('../db');
+const app = require('../../app');
+const { pgPool } = require('../../db');
 
 describe('Test all middlewares and routes', () => {
   
--- backend/tests/queries.js
+++
@@ -1,7 +0,0 @@
-const queries = {
-  INSERT_TASK_QUERY: 'select * from insert_task($1, $2, $3, $4, $5)',
-  INSERT_ASSET_QUERY: 'select * from insert_asset($1)',
-  INSERT_PERSON_QUERY: 'select * from insert_person($1, $2)',
-};
-
-module.exports = queries;
