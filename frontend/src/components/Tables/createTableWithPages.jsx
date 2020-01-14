6315a4252a9546953505830b0fecb58f98d73560
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-01-14 16:34:02 -0300

    Add parameters for createTableWithPages

---
+++ frontend/src/components/Tables/createTableWithPages.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const testObject = {
+  numberOfColumns: 10,
+  checkbox: true,
+  columnObjects: [
+    { name: '1', description: 'a', className: 'b', style: 'c', data: ['id1', 'id2'], dataConfig: {} },
+    { name: '2', description: 'd', className: 'b', style: 'c', data: ['id3'], dataConfig: {} },
+  ],
+  data: data,
+}
+
+function createTableWithPages() {
+
+}
