3d8a0d29ede180c87f749396f6b56384abbabec7
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-01-14 22:29:12 -0300

    Detach table header and table body from containers

--- frontend/src/components/Tables/createTableWithPages.jsx
+++ frontend/src/components/Tables/createTableWithPages.jsx
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
+import { CustomInput } from 'reactstrap';
 
-const testObject = {
+const testTableConfig = {
   numberOfColumns: 10,
   checkbox: true,
   columnObjects: [
-    { name: '1', description: 'a', className: 'b', style: 'c', data: ['id1', 'id2'], dataConfig: {} },
-    { name: '2', description: 'd', className: 'b', style: 'c', data: ['id3'], dataConfig: {} },
+    { name: '1', description: 'a', className: 'b', style: 'c', data: ['id1', 'id2']},
+    { name: '2', description: 'd', className: 'b', style: 'c', data: ['id3']},
   ],
-  data: data,
+};
+
+function TableHeader({ tableConfig }) {
+  return (
+    <tr>
+      <th className="text-center checkbox-cell">
+        <CustomInput type="checkbox" />
+      </th>
+      {tableConfig.columnObjects.map((column) => (
+        <th style={column.style} className={column.className}>column.description</th>
+      ))}
+    </tr>
+  );
+};
+
+function TableBody({ tableConfig, data }) {
+  return (
+    data.map((item) => (
+      <tr>
+        <td className="text-center checkbox-cell">
+          <CustomInput type="checkbox" />
+        </td>
+        {tableConfig.columnObjects.map((column) => (
+          column.data.length >= 2 
+          ? <td>
+              <div>{item[column.data[0]]}</div>
+              <div className="small text-muted" >{item[column.data[1]]}</div>
+            </td>
+          : <td className="text-center">{item[column.name]}</td>
+        ))}
+      </tr>
+    ))
+  );
 }
 
-function createTableWithPages() {
+function createTableWithPages({ tableConfig, data }) {
+  return (
 
+  );
 }
