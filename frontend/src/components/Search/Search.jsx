8ad07c69d8afb601384f772684f97d41acf240d3
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-01-15 15:01:39 -0300

    Rename SearchUI to Search

--- frontend/src/components/Search/Search.jsx
+++ frontend/src/components/Search/Search.jsx
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
-import SearchUI from './SearchUI';
+import "./Search.css";
+
+const searchImage = require("../../assets/icons/search_icon.png");
 
 export default class Search extends Component {
   render() {
-    return (  );
+    const {
+      searchTerm,
+      handleChangeSearchTerm,
+      filterLogic,
+      filterName,
+      numberOfItens,
+      toggleApply,
+      toggleCreate
+    } = this.props;
+
+    return (
+      <div className="card-search-container">
+        <div className="search" style={{ width: "30%" }}>
+          <div className="card-search-form">
+            <InputGroup>
+              <Input placeholder="Pesquisar ..." value={searchTerm} onChange={handleChangeSearchTerm} />
+              <InputGroupAddon addonType="append">
+                <InputGroupText><img src={searchImage} alt="" style={{ width: "19px", height: "16px", margin: "3px 0px" }} /></InputGroupText>
+              </InputGroupAddon>
+            </InputGroup>
+          </div>
+        </div>
+        <div className="search-filter" style={{ width: "30%" }}>
+          <ol>
+            <li><span className="card-search-title">Filtro: </span></li>
+            <li><span className="card-search-title">Resultado: </span></li>
+          </ol>
+          <ol>
+            <li>{(filterLogic.length === 0) ? "Sem filtro" : (filterName || "Filtro sem nome")}</li>
+            <li><span>Página com </span><b>{numberOfItens.toString()}</b><span> itens</span></li>
+          </ol>
+        </div>
+        <div className="search-buttons" style={{ width: "30%" }}>
+          <Button className="search-filter-button" color="success" onClick={toggleApply}>Aplicar Filtro</Button>
+          <Button className="search-filter-button" color="primary" onClick={toggleCreate}>Criar Filtro</Button>
+        </div>
+      </div>
+    );
   }
 }
