a231d2b78ee4c17b1a5012b1adde2d07d3252501
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-01-21 14:26:12 -0300

    Isolate itemsMatrix on Maintenance Tab

--- frontend/src/containers/Assets/Facility/TabsElements/MaintenanceTab.jsx
+++ frontend/src/containers/Assets/Facility/TabsElements/MaintenanceTab.jsx
@@ -1,39 +1,6 @@
 import React, { Component } from 'react';
 import DescriptionTable from '../../../../components/Description/DescriptionTable';
-import { FormGroup, CustomInput } from 'reactstrap';
-
-function itemsMatrixReport(data) {
-  return (
-    [
-      [
-        { id: 'unsettledMaintenance', title: 'Manutenções Pendentes', description: '0000', span: 1 },
-        { id: 'prevent', title: 'Preventivas Agendadas', description: 'OS-0021 / OS-0025', line: 1, span: 1 },
-      ],
-      [
-        { id: 'totalMaintenance', title: 'Manutenções Totais', description: '0000', span: 1 },
-        { id: 'forAnaylsis', title: 'Requisições para Análise', description: 'RQ-0011 / RQ-0015', line: 1, span: 1 },
-      ],
-    ]
-  );
-}
-
-function itemsMatrixMaintenance(data) {
-  return (
-    [
-      [
-        { id: 'parentAssets', title: 'Ativos Filhos Incluídos', description: 'Não', span: 1 },
-        {
-          id: 'includeAssets', elementGenerator: () =>
-            (
-              <FormGroup style={{ margin: "0" }}>
-                <CustomInput disabled type="switch" id="child-assets" name="customSwitch" label="Incluir ativos filhos" />
-              </FormGroup>
-            ), line: 1, span: 1, style: { display: "flex", alignItems: "flex-end" }
-        },
-      ],
-    ]
-  );
-}
+import { itemsMatrixReport, itemsMatrixMaintenance } from '../utils/descriptionMatrix';
 
 class MaintenanceTab extends Component {
   render() {
