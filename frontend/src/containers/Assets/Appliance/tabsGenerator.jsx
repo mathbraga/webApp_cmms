16d83bf52da5c32d93412ad0ab515004372a3d36
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-01-21 17:49:08 -0300

    Create Appliance Views and Components

--- frontend/src/containers/Assets/Appliance/tabsGenerator.jsx
+++ frontend/src/containers/Assets/Appliance/tabsGenerator.jsx
@@ -3,6 +3,7 @@ import InfoTab from './TabsElements/InfoTab';
 import LocationTab from './TabsElements/LocationTab';
 import MaintenanceTab from './TabsElements/MaintenanceTab';
 import AssetTab from './TabsElements/AssetTab';
+import WarrantyTab from './TabsElements/WarrantyTab';
 import FilesTab from './TabsElements/FilesTab';
 import LogTab from './TabsElements/LogTab';
 
@@ -10,8 +11,9 @@ export default function tabsGenerator(data) {
   return (
     [
       { id: 'info', title: 'Informações Gerais', element: <InfoTab data={data} /> },
-      { id: 'location', title: 'Planta Arquitetônica', element: <LocationTab data={data} /> },
-      { id: 'maintenance', title: 'Manutenções', element: <MaintenanceTab data={data} /> },
+      { id: 'location', title: 'Localização', element: <LocationTab data={data} /> },
+      { id: 'maintenance', title: 'Manutenção', element: <MaintenanceTab data={data} /> },
+      { id: 'warranty', title: 'Garantia', element: <WarrantyTab data={data} /> },
       { id: 'asset', title: 'Ativos', element: <AssetTab data={data} /> },
       { id: 'files', title: 'Arquivos', element: <FilesTab data={data} /> },
       { id: 'log', title: 'Histórico', element: <LogTab data={data} /> },
--- frontend/src/containers/Assets/Appliance/utils/descriptionMatrix.js
+++ frontend/src/containers/Assets/Appliance/utils/descriptionMatrix.js
@@ -1,6 +1,50 @@
 import React, { Component } from 'react';
 import { FormGroup, CustomInput } from 'reactstrap';
 
+export function itemsMatrixGeneral(data) {
+  return (
+    [
+      [
+        { id: 'appliance', title: 'Equipamento', description: data.name, span: 1 },
+        { id: 'serial', title: 'Número Serial', description: data.serial, line: 1, span: 1 },
+      ],
+      [
+        { id: 'id', title: 'Código', description: data.assetSf, span: 1 },
+        { id: 'price', title: 'Preço', description: data.price, line: 1, span: 1 },
+      ],
+      [{ id: 'model', title: 'Modelo', description: data.model, span: 1 }],
+    ]
+  );
+}
+
+export function itemsMatrixManufacturer(data) {
+  return (
+    [
+      [
+        { id: 'manufacturer', title: 'Empresa Fabricante', description: data.manufacturer, span: 1 },
+        { id: 'phone', title: 'Telefone', description: data.phoneManufacturer, line: 1, span: 1 },
+      ],
+      [
+        { id: 'city', title: 'Cidade', description: data.cityManufacturer, span: 1 },
+        { id: 'email', title: 'E-mail', description: data.email, line: 1, span: 1 },
+      ],
+    ]
+  );
+}
+
+export function itemsMatrixParent(data) {
+  return (
+    [
+      [
+        { id: 'parent', title: 'Nome do Equipamento', description: data.parent, span: 1 },
+      ],
+      [
+        { id: 'parentId', title: 'Código', description: data.parentId, span: 1 },
+      ],
+    ]
+  );
+}
+
 export function itemsMatrixReport(data) {
   return (
     [
