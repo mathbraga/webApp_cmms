e8f3332bdf66e81ad8c0eb6880cde4486dbaa183
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-01-22 11:29:33 -0300

    Create MaterialTab for Task

--- frontend/src/containers/Maintenance/Task/TabsElements/MaterialTab.jsx
+++ frontend/src/containers/Maintenance/Task/TabsElements/MaterialTab.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import DescriptionTable from '../../../../components/Description/DescriptionTable';
+import { itemsMatrixMaterial } from '../utils/descriptionMatrix';
 import './Tabs.css';
 
 class MaterialTab extends Component {
   state = {}
   render() {
+    const data = this.props.data.allOrderSuppliesDetails.nodes;
+    console.log("Material: ", data);
     return (
-      <h1>HI!!!!</h1>
+      <>
+        <DescriptionTable
+          title={'Lista de Materiais e Serviços'}
+          numColumns={2}
+          itemsMatrix={itemsMatrixMaterial(data)}
+        />
+        {/* <CardWithTable
+          tableConfig={tableConfig}
+          customFilters={customFilters}
+          filterAttributes={filterAttributes}
+          searchableAttributes={searchableAttributes}
+          hasAssetCard={false}
+          data={data}
+        /> */}
+      </>
     );
   }
 }
--- frontend/src/containers/Maintenance/Task/utils/descriptionMatrix.js
+++ frontend/src/containers/Maintenance/Task/utils/descriptionMatrix.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
-import { FormGroup, CustomInput } from 'reactstrap';
 import { ORDER_CATEGORY_TYPE, ORDER_STATUS_TYPE, ORDER_PRIORITY_TYPE } from '../../Tasks/utils/dataDescription';
 
 export function itemsMatrixGeneral(data) {
   return (
     [
       [
-        { id: 'appliance', title: 'Título do Serviço', description: data.orderByOrderId.title, span: 1 },
-        { id: 'serial', title: 'Status', description: ORDER_STATUS_TYPE[data.orderByOrderId.status], span: 1 },
+        { id: 'title', title: 'Título do Serviço', description: data.orderByOrderId.title, span: 1 },
+        { id: 'status', title: 'Status', description: ORDER_STATUS_TYPE[data.orderByOrderId.status], span: 1 },
       ],
       [
-        { id: 'id', title: 'Ordem de Serviço nº', description: data.orderByOrderId.orderId.toString().padStart(4, "0"), span: 1 },
-        { id: 'price', title: 'Executado (%)', description: data.orderByOrderId.progress, span: 1 },
+        { id: 'orderId', title: 'Ordem de Serviço nº', description: data.orderByOrderId.orderId.toString().padStart(4, "0"), span: 1 },
+        { id: 'progress', title: 'Executado (%)', description: data.orderByOrderId.progress, span: 1 },
       ],
       [
-        { id: 'appliance', title: 'Categoria', description: ORDER_CATEGORY_TYPE[data.orderByOrderId.category], span: 1 },
-        { id: 'serial', title: 'Prioridade', description: ORDER_PRIORITY_TYPE[data.orderByOrderId.priority], span: 1 },
+        { id: 'category', title: 'Categoria', description: ORDER_CATEGORY_TYPE[data.orderByOrderId.category], span: 1 },
+        { id: 'priority', title: 'Prioridade', description: ORDER_PRIORITY_TYPE[data.orderByOrderId.priority], span: 1 },
       ],
-      [{ id: 'model', title: 'Local', description: data.orderByOrderId.place, span: 1 }],
-      [{ id: 'model', title: 'Descrição Técnica', description: data.orderByOrderId.description, span: 2 }],
+      [{ id: 'place', title: 'Local', description: data.orderByOrderId.place, span: 1 }],
+      [{ id: 'description', title: 'Descrição Técnica', description: data.orderByOrderId.description, span: 2 }],
     ]
   );
 }
@@ -27,14 +25,22 @@ export function itemsMatrixDate(data) {
   return (
     [
       [
-        { id: 'manufacturer', title: 'Criação da OS', description: data.orderByOrderId.createdAt, span: 1 },
-        { id: 'phone', title: 'Prazo Final', description: data.orderByOrderId.dateLimit, span: 1 },
+        { id: 'createdAt', title: 'Criação da OS', description: data.orderByOrderId.createdAt, span: 1 },
+        { id: 'dateLimit', title: 'Prazo Final', description: data.orderByOrderId.dateLimit, span: 1 },
       ],
       [
-        { id: 'city', title: 'Início da Execução', description: data.orderByOrderId.dateStart, span: 1 },
-        { id: 'email', title: 'Dias de Atraso', description: data.orderByOrderId.dateLimit, span: 1 },
+        { id: 'dateStart', title: 'Início da Execução', description: data.orderByOrderId.dateStart, span: 1 },
+        { id: 'delay', title: 'Dias de Atraso', description: data.orderByOrderId.dateLimit, span: 1 },
       ],
-      [{ id: 'model', title: 'Término da Execução', description: data.orderByOrderId.dateEnd, span: 1 }],
+      [{ id: 'dateEnd', title: 'Término da Execução', description: data.orderByOrderId.dateEnd, span: 1 }],
+    ]
+  );
+}
+
+export function itemsMatrixMaterial(data) {
+  return (
+    [
+      [{ id: 'quantity', title: 'Quantidade', description: data.length.toString().padStart(3, "0"), span: 1 }],
     ]
   );
 }
