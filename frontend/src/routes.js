ebe55ad1f07ab57f1013c84c2c852a439f180c1e
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-01-22 22:54:52 -0300

    Adjust routes.js file

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,9 @@
 import React from "react";
 import MainPage from "./containers/MainPage";
-import paths from "./paths";
 
-const ConsumptionMonitor = React.lazy(() => import("./containers/ConsumptionMonitor"));
 const Dashboard = React.lazy(() => import("./containers/Dashboard"));
 const Login = React.lazy(() => import("./containers/Authentication/Login"));
-const Error404 = React.lazy(() => import("./containers/MainPage/Error404"));
+const Profile = React.lazy(() => import("./containers/Authentication/Profile"));
 const Facilities = React.lazy(() => import("./containers/Assets/Facilities/Facilities"));
 const Facility = React.lazy(() => import("./containers/Assets/Facility/Facility"));
 const Appliances = React.lazy(() => import("./containers/Assets/Appliances/Appliances"));
@@ -19,48 +17,31 @@ const Spec = React.lazy(() => import("./containers/ContractManagement/Specificat
 const Teams = React.lazy(() => import("./containers/HumanResources/Teams/Teams"));
 const Persons = React.lazy(() => import("./containers/HumanResources/Persons/Persons"));
 
-// OLD ROUTES
-// const RegisterUser = React.lazy(() => import("./containers/Authentication/RegisterUser"));
-const Profile = React.lazy(() => import("./containers/Authentication/Profile"));
-const Assets = React.lazy(() => import("./containers/Assets"));
-const WorkOrders = React.lazy(() => import("./containers/Maintenance/WorkOrders"));
-const WorkOrderView = React.lazy(() => import("./containers/Maintenance/WorkOrderView"));
-const ContractView = React.lazy(() => import("./containers/Contracts/ContractView"));
-const SpecView = React.lazy(() => import("./containers/Contracts/SpecView"));
-const AssetInfo = React.lazy(() => import("./containers/Assets/AssetInfo"));
-const FacilitiesForm = React.lazy(() => import("./containers/Assets/FacilitiesForm"));
-const EquipmentsForm = React.lazy(() => import("./containers/Assets/EquipmentsForm"));
-const OrderForm = React.lazy(() => import("./containers/Maintenance/OrderForm"));
-const GroupView = React.lazy(() => import("./containers/Teams/GroupView"));
-const PersonView = React.lazy(() => import("./containers/Teams/PersonView"));
+const NoView = <h1>NoView!</h1>
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
-  { path: paths.HOME, exact: true, component: MainPage },
-  { path: paths.DASHBOARD, name: "Painel", component: Dashboard },
+  { path: '/', exact: true, name: "Página Principal", component: MainPage },
+  { path: '/painel', name: "Painel", component: Dashboard },
   { path: "/login", name: "Login", component: Login },
-  // { path: "/cadastro", name: "Cadastro", component: RegisterUser },
   { path: "/perfil", name: "Perfil", component: Profile },
-  { path: "/energia", name: "Energia elétrica", component: ConsumptionMonitor },
-  { path: "/agua", name: "Água", component: ConsumptionMonitor },
+  { path: "/ativos/edificios", exact: true, name: "Edifícios", component: Facilities },
   { path: "/ativos/edificio/view/:id", exact: false, name: "Edifício", component: Facility },
+  { path: "/ativos/edificios/novo", exact: true, name: "Novo Edificio", component: NoView },
+  { path: "/ativos/equipamentos", exact: true, name: "Equipamentos", component: Appliances },
   { path: "/ativos/equipamento/view/:id", exact: false, name: "Equipamento", component: Appliance },
-  { path: "/ativos/edificios", exact: true, name: "Ativos", component: Facilities },
-  { path: "/ativos/equipamentos", exact: true, name: "Ativos", component: Appliances },
-  { path: "/ativos/edificios/novo", exact: true, name: "Novo Edificio", component: FacilitiesForm },
-  { path: "/ativos/equipamentos/novo", exact: true, name: "Novo Equipamento", component: EquipmentsForm },
+  { path: "/ativos/equipamentos/novo", exact: true, name: "Novo Equipamento", component: NoView },
   { path: "/manutencao/os", exact: true, name: "Ordens de serviços", component: Tasks },
-  { path: "/manutencao/os/nova", exact: true, name: "Nova OS", component: OrderForm },
-  { path: "/manutencao/os/view/:id", exact: false, name: "OS", component: Task },
+  { path: "/manutencao/os/view/:id", exact: false, name: "Ordem de Serviço", component: Task },
+  { path: "/manutencao/os/nova", exact: true, name: "Nova OS", component: NoView },
   { path: "/gestao/contratos", exact: true, name: "Contratos", component: Contracts },
   { path: "/gestao/contratos/view/:id", exact: false, name: "Contrato", component: Contract },
   { path: "/gestao/servicos", exact: true, name: "Servicos", component: Specs },
   { path: "/gestao/servicos/view/:id", exact: false, name: "Servico", component: Spec },
-  { path: "/equipes/grupos", exact: true, name: "Servicos", component: Teams },
-  { path: "/equipes/grupo/view/:id", exact: false, name: "Servico", component: GroupView },
-  { path: "/equipes/pessoas", exact: true, name: "Servicos", component: Persons },
-  { path: "/equipes/pessoa/view/:id", exact: false, name: "Servico", component: PersonView },
-  { path: "/erro404", exact: true, name: "Erro 404", component: Error404 },
+  { path: "/equipes/grupos", exact: true, name: "Grupos", component: Teams },
+  { path: "/equipes/grupo/view/:id", exact: false, name: "Grupo", component: NoView },
+  { path: "/equipes/pessoas", exact: true, name: "Pessoas", component: Persons },
+  { path: "/equipes/pessoa/view/:id", exact: false, name: "Pessoa", component: NoView },
 ];
 
 export default routes;
