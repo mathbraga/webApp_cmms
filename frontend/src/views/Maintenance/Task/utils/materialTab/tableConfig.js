d195e805736485d49348fedb7e49939fe5cd1d02
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-01-27 17:54:53 -0300

    Adjust tableConfig on materialTab for Task

--- frontend/src/views/Maintenance/Task/utils/materialTab/tableConfig.js
+++ frontend/src/views/Maintenance/Task/utils/materialTab/tableConfig.js
@@ -7,7 +7,7 @@ const tableConfig = {
   itemClickable: false,
   idAttributeForData: 'supplyId',
   columnObjects: [
-    { name: 'supplyId', description: 'Código', style: { width: "40px" }, className: "text-center", data: ['supplyId'] },
+    { name: 'supplySf', description: 'Código', style: { width: "40px" }, className: "text-center", data: ['supplySf'] },
     { name: 'name', description: 'Descrição', style: { width: "200px" }, className: "text-justify", data: ['name'] },
     { name: 'qty', description: 'Quantidade', style: { width: "50px" }, className: "text-center", data: ['qty'] },
     { name: 'bidPrice', description: 'Preço Unitário', style: { width: "70px" }, className: "text-center", data: ['bidPrice'], dataGenerator: (item) => ([formatter(item.bidPrice)]) },
