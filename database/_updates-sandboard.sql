6f43ee86e1be04f95f55a6c50d91898e1085f1aa
hzlopes <hzlopes@senado.leg.br>
2020-01-27 16:11:20 -0300

    More more views

--- database/_updates-sandboard.sql
+++ database/_updates-sandboard.sql
@@ -1,5 +1,39 @@
 begin;
 
--- todo
+drop view spec_data;
+
+create view supplies_of_spec as
+  select z.spec_id,
+         jsonb_agg(jsonb_build_object(
+           'supplyId', s.supply_id,
+           'supplySf', s.supply_sf,
+           'contractId', c.contract_id,
+           'contractSf', c.contract_sf,
+           'title', c.title,
+           'qtyInitial', b.qty_initial,
+           'qtyBlocked', b.qty_blocked,
+           'qtyConsumed', b.qty_consumed,
+           'qtyAvailable', b.qty_available
+         )) as supplies
+    from specs as z
+    left join supplies as s using (spec_id)
+    left join contracts as c using (contract_id)
+    left join balances as b using (supply_id)
+  group by z.spec_id
+;
+
+create view spec_data as
+  select z.*,
+         zc.spec_category_text,
+         zs.spec_subcategory_text,
+         s.supplies
+    from specs as z
+    inner join spec_categories as zc using (spec_category_id)
+    inner join spec_subcategories as zs using (spec_subcategory_id)
+    left join supplies_of_spec as s using (spec_id)
+;
+
+
+select supplies from spec_data;
 
 rollback;
--- database/_updates.sql
+++ database/_updates.sql
@@ -84,22 +84,33 @@ create view appliance_data as
     inner join tasks_of_asset as t on (a.asset_id = t.asset_id)
 ;
 
+create view supplies_of_contract as
+  select s.contract_id,
+         jsonb_agg(jsonb_build_object(
+           'supplyId', s.supply_id,
+           'supplySf', s.supply_sf,
+           'name', z.name,
+           'unit', z.unit,
+           'qtyInitial', b.qty_initial,
+           'bidPrice', s.bid_price,
+           'qtyConsumed', b.qty_consumed,
+           'qtyBlocked', b.qty_blocked,
+           'qtyAvailable', b.qty_available
+         )) as supplies
+    from supplies as s
+    inner join specs as z using (spec_id)
+    inner join balances as b using (supply_id)
+  group by s.contract_id
+;
+
 create view contract_data as
   select c.*,
-         cs.contract_status_text
+         cs.contract_status_text,
+         s.supplies
     from contracts as c
     inner join contract_statuses as cs using (contract_status_id)
+    inner join supplies_of_contract as s using (contract_id)
 ;
-
-create view spec_data as
-  select z.*,
-         zc.spec_category_text,
-         zs.spec_subcategory_text
-    from specs as z
-    inner join spec_categories as zc using (spec_category_id)
-    inner join spec_subcategories as zs using (spec_subcategory_id)
-;
-
 
 create view team_data as
   select t.team_id, 
@@ -134,3 +145,33 @@ create view person_data as
 ;
 
 
+create view supplies_of_spec as
+  select z.spec_id,
+         jsonb_agg(jsonb_build_object(
+           'supplyId', s.supply_id,
+           'supplySf', s.supply_sf,
+           'contractId', c.contract_id,
+           'contractSf', c.contract_sf,
+           'title', c.title,
+           'qtyInitial', b.qty_initial,
+           'qtyBlocked', b.qty_blocked,
+           'qtyConsumed', b.qty_consumed,
+           'qtyAvailable', b.qty_available
+         )) as supplies
+    from specs as z
+    left join supplies as s using (spec_id)
+    left join contracts as c using (contract_id)
+    left join balances as b using (supply_id)
+  group by z.spec_id
+;
+
+create view spec_data as
+  select z.*,
+         zc.spec_category_text,
+         zs.spec_subcategory_text,
+         s.supplies
+    from specs as z
+    inner join spec_categories as zc using (spec_category_id)
+    inner join spec_subcategories as zs using (spec_subcategory_id)
+    left join supplies_of_spec as s using (spec_id)
+;
