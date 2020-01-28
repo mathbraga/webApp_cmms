34dcd921efdf132ef518a694bfa0dfd5f05a588c
hzlopes <hzlopes@senado.leg.br>
2020-01-28 11:20:20 -0300

    Create tasks_of_spec view.

--- database/_updates.sql
+++ database/_updates.sql
@@ -169,14 +169,34 @@ create view supplies_of_spec as
   group by z.spec_id
 ;
 
+create view tasks_of_spec as
+  select z.spec_id,
+         jsonb_agg(jsonb_build_object(
+           'taskId', t.task_id,
+           'title', t.title,
+           'place', t.place,
+           'taskCategoryText', tc.task_category_text,
+           'taskStatusText', tz.task_status_text
+         )) as tasks
+    from specs as z
+    inner join supplies as s using (spec_id)
+    inner join task_supplies as ts using (supply_id)
+    inner join tasks as t using (task_id)
+    inner join task_statuses as tz using (task_status_id)
+    inner join task_categories as tc using (task_category_id)
+  group by z.spec_id
+;
+
 create view spec_data as
   select z.*,
          zc.spec_category_text,
          zs.spec_subcategory_text,
          s.total_available,
-         s.supplies
+         s.supplies,
+         t.tasks
     from specs as z
     inner join spec_categories as zc using (spec_category_id)
     inner join spec_subcategories as zs using (spec_subcategory_id)
     left join supplies_of_spec as s using (spec_id)
+    left join tasks_of_spec as t using (spec_id)
 ;
