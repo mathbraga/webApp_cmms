9b17b62237470527313b71449e8826e0e53e2d65
Matheus Braga <matheus_ob59@hotmail.com>
2020-01-28 11:56:02 -0300

    Import type variables.

--- frontend/src/utils/search/searchList.js
+++ frontend/src/utils/search/searchList.js
@@ -1,39 +1,10 @@
 // Function used for search mechanics on search dependent components
 
-const ORDER_CATEGORY_TYPE = {
-  'EST': 'Avaliação estrutural',
-  'FOR': 'Reparo em forro',
-  'INF': 'Infiltração',
-  'ELE': 'Instalações elétricas',
-  'HID': 'Instalações hidrossanitárias',
-  'MAR': 'Marcenaria',
-  'PIS': 'Reparo em piso',
-  'REV': 'Revestimento',
-  'VED': 'Vedação espacial',
-  'VID': 'Vidraçaria / Esquadria',
-  'SER': 'Serralheria',
-  'ARC': 'Ar-condicionado',
-  'ELV': 'Elevadores',
-  'EXA': 'Exaustores',
-  'GRL': 'Serviços Gerais',
-};
-
-const ORDER_STATUS_TYPE = {
-  'CAN': 'Cancelada',
-  'NEG': 'Negada',
-  'PEN': 'Pendente',
-  'SUS': 'Suspensa',
-  'FIL': 'Fila de espera',
-  'EXE': 'Execução',
-  'CON': 'Concluída',
-}
-
-const ORDER_PRIORITY_TYPE = {
-  'BAI': 'Baixa',
-  'NOR': 'Normal',
-  'ALT': 'Alta',
-  'URG': 'Urgente',
-};
+import {
+  ORDER_CATEGORY_TYPE, 
+  ORDER_STATUS_TYPE, 
+  ORDER_PRIORITY_TYPE 
+} from "../../views/Maintenance/Tasks/utils/dataDescription";
 
 export default function searchList(itemsList, attributes, searchTerm) {
   let filteredItems = itemsList;
