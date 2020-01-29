e52c578a8c6196548e46c8cc8fdd6c30f451b5c9
Matheus Braga <matheus_ob59@hotmail.com>
2020-01-29 13:30:08 -0300

    Include asset relations in facility_data view.

--- database/_sandbox.sql
+++ database/_sandbox.sql
@@ -1,5 +1,22 @@
 begin;
 
--- todo
+create view facility_children as 
+  select a.asset_id,
+         a.asset_sf,
+         a.name,
+         a.description,
+         aa.name as category_name,
+         a.latitude,
+         a.longitude,
+         a.area,
+         t.tasks,
+         at.parent_id,
+         at.assets
+    from assets as a
+    inner join assets as aa on (a.category = aa.asset_id)
+    left join tasks_of_asset as t on (a.asset_id = t.asset_id)
+    left join get_asset_tree(a.asset_id) as at on (a.asset_id = at.parent_id)
+  where a.category = 1
+;
 
 rollback;
