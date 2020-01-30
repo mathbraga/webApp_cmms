6dbc4af1130c1bbab00c2011f04ad13ed7f7189c
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-01-30 13:48:49 -0300

    Create DescriptionForm on TaskForm

--- frontend/src/views/Maintenance/TaskForm/TaskForm.jsx
+++ frontend/src/views/Maintenance/TaskForm/TaskForm.jsx
@@ -1,11 +1,72 @@
 import React, { Component } from 'react';
+import AssetCard from '../../../components/Cards/AssetCard';
+import TextField from '@material-ui/core/TextField';
+import withDataFetching from '../../../components/DataFetch';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextareaAutosize, MenuItem } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'redux';
+import './TaskForm.css';
+import {
+  Form,
+  Row,
+  Col,
+  FormGroup,
+  Label,
+  Input
+} from 'reactstrap';
+
+import { fetchGQL, fetchVariables } from './utils/dataFetchParameters';
+import DescriptionForm from './formParts/DescriptionForm';
 
 class TaskForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      place: "",
+      description: "",
+      priority: 2,
+      category: null,
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
+    const { history } = this.props;
+    const data = this.props.data.allTaskFormData.nodes[0];
     return (
-      <div>Formulário!</div>
+      <CssBaseline>
+        <AssetCard
+          sectionName={'Cadastro de OS'}
+          sectionDescription={'Formulário para cadastro de novas ordens de serviço'}
+          handleCardButton={() => { history.push("/manutencao/os") }}
+          buttonName={'Ordens de Serviço'}
+        >
+          <div className="input-container">
+            <DescriptionForm
+              handleInputChange={this.handleInputChange}
+              priorityOptions={data.priorityOptions}
+              categoryOptions={data.categoryOptions}
+              {...this.state}
+            />
+          </div>
+        </AssetCard>
+      </CssBaseline>
     );
   }
 }
 
-export default TaskForm;
+export default compose(
+  withRouter,
+  withDataFetching(fetchGQL, fetchVariables)
+)(TaskForm);
---
+++ frontend/src/views/Maintenance/TaskForm/formParts/DescriptionForm.jsx
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import { MenuItem } from '@material-ui/core';
+import {
+  Row,
+  Col,
+} from 'reactstrap';
+
+class DescriptionForm extends Component {
+  render() {
+    const {
+      handleInputChange,
+      priorityOptions,
+      categoryOptions
+    } = this.props;
+    return (
+      <>
+        <h1 className="input-container-title">Descrição do Serviço</h1>
+        <Row>
+          <Col md={8}>
+            <TextField
+              id="outlined-full-width"
+              value={this.props.title}
+              className="text-input"
+              name="title"
+              fullWidth
+              label="Título"
+              placeholder="Instalação de um novo ponto de energia elétrica"
+              onChange={handleInputChange}
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+            />
+          </Col>
+          <Col md={4}>
+            <TextField
+              id="outlined-full-width"
+              value={this.props.priority}
+              className="text-input"
+              name="priority"
+              select
+              fullWidth
+              label="Prioridade"
+              margin="normal"
+              onChange={handleInputChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined">
+              {
+                priorityOptions.map(option => (
+                  <MenuItem key={option.taskPriorityId} value={option.taskPriorityId}>
+                    {option.taskPriorityText}
+                  </MenuItem>
+                ))
+              }
+            </TextField>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={8}>
+            <TextField
+              id="outlined-full-width"
+              value={this.props.place}
+              className="text-input"
+              name="place"
+              fullWidth
+              label="Local"
+              placeholder="Auditório do Interlegis"
+              onChange={handleInputChange}
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+            />
+          </Col>
+          <Col md={4}>
+            <TextField
+              id="outlined-full-width"
+              value={this.props.category}
+              className="text-input"
+              name="category"
+              select
+              fullWidth
+              label="Categoria"
+              margin="normal"
+              onChange={handleInputChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined">
+              {categoryOptions.map(option => (
+                <MenuItem key={option.taskCategoryId} value={option.taskCategoryId}>
+                  {option.taskCategoryText}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Col>
+        </Row>
+        <Row style={{ marginTop: "15px" }}>
+          <Col>
+            <TextField
+              id="outlined-multiline-static"
+              value={this.props.description}
+              className="text-input"
+              name="description"
+              label="Descrição"
+              placeholder="Instalação de um novo ponto de energia elétrica para alimentar um computador."
+              fullWidth
+              multiline
+              onChange={handleInputChange}
+              rows="6"
+              rowsMax="20"
+              variant="outlined"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Col>
+        </Row>
+      </>
+    );
+  }
+}
+
+export default DescriptionForm;
