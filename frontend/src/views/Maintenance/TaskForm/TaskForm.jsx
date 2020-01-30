255f4a113958239ff2f990fb45fcd06e5fb0366c
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-01-30 09:45:12 -0300

    Create TaskForm component

---
+++ frontend/src/views/Maintenance/TaskForm/TaskForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TaskForm extends Component {
+  render() {
+    return (
+      <div>Formulário!</div>
+    );
+  }
+}
+
+export default TaskForm;
