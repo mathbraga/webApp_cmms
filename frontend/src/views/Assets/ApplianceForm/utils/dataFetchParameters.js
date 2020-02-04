bd873a63a9d7cc0658d46ef3a4664f02ac25b931
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-02-04 14:32:49 -0300

    Fix dataFetchParameters on ApplianceForm

--- frontend/src/views/Assets/ApplianceForm/utils/dataFetchParameters.js
+++ frontend/src/views/Assets/ApplianceForm/utils/dataFetchParameters.js
@@ -1,11 +1,26 @@
 import gql from 'graphql-tag';
 
 const fetchGQL = gql`
-  query MyQuery {
+  query MyQuery($assetId: Int!) {
     allAssetFormData {
       nodes {
         topOptions
         parentOptions
+      }
+    }
+    allApplianceData(condition: {assetId: $assetId}) {
+      nodes {
+        assetId
+        assetSf
+        contexts
+        description
+        manufacturer
+        model
+        name
+        parents
+        price
+        serialnum
+        tasks
       }
     }
   }
