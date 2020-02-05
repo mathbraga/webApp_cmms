99d8c81f778dc3be071d6010288ede23b0b48bba
hzlopes <hzlopes@senado.leg.br>
2020-02-05 14:18:23 -0300

    Add mutation GQLs for Facility Form

--- frontend/src/views/Assets/FacilityForm/utils/mutationParameters.js
+++ frontend/src/views/Assets/FacilityForm/utils/mutationParameters.js
@@ -2,16 +2,33 @@ import gql from 'graphql-tag';
 
 const mutationGQLEdit = gql`
   mutation (
-
+    $attributes: AssetInput!,
+    $tops: [Int!]!,
+    $parents: [Int!]!
   ) modifyAsset {
-    
+    input: (
+      attributes: $attributes
+      tops: $tops
+      parents: $parents
+    ) {
+      result
+    }
   }
 `;
 
 const mutationGQLNew = gql`
   mutation (
-
+    $attributes: AssetInput!,
+    $tops: [Int!]!,
+    $parents: [Int!]!
   ) insertAsset {
+    input: (
+      attributes: $attributes
+      tops: $tops
+      parents: $parents
+    ) {
+      result
+    }
   }
 `;
 
