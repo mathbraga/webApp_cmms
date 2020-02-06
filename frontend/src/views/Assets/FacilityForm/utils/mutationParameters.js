44a6396050651f12da21bb9c16324304344eefbc
hzlopes <hzlopes@senado.leg.br>
2020-02-06 10:56:55 -0300

    Fix gql indentation

--- frontend/src/views/Assets/FacilityForm/utils/mutationParameters.js
+++ frontend/src/views/Assets/FacilityForm/utils/mutationParameters.js
@@ -6,7 +6,7 @@ const mutationGQLEdit = gql`
     $tops: [Int!]!,
     $parents: [Int!]!
   ) {
-  mutationResponse:  modifyAsset (
+    mutationResponse:  modifyAsset (
       input: {
         attributes: $attributes
         tops: $tops
@@ -24,7 +24,7 @@ const mutationGQLNew = gql`
     $tops: [Int!]!,
     $parents: [Int!]!
   ) {
-  mutationResponse:  insertAsset (
+    mutationResponse:  insertAsset (
       input: {
         attributes: $attributes
         tops: $tops
