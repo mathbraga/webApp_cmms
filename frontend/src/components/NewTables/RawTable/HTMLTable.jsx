27febd63b441f3b7b7cd3a4fe76d5420164dfa33
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-02-13 16:09:00 -0300

    Create layout for table with no items

--- frontend/src/components/NewTables/RawTable/HTMLTable.jsx
+++ frontend/src/components/NewTables/RawTable/HTMLTable.jsx
@@ -35,108 +35,115 @@ class HTMLTable extends Component {
     const { onSort, activeSortKey, visibleData, tableConfig, selectedData, isSortReverse } = this.props;
     return (
       <div className="table-wrapper">
-        <table className="main-table">
-          <thead className="main-table__header">
-            <tr
-              className="main-table__header__row"
-              key="header"
-            >
-              {tableConfig.checkbox && (
-                <th
-                  className={classNames({
-                    "main-table__head": true,
-                    "main-table__head--center": true,
-                  })}
-                  style={{ width: tableConfig.checkboxWidth || "5%" }}
-                  key={"checkbox"}
+        {visibleData.length === 0
+          ? (
+            <div className="table-no-item">Página sem itens.</div>
+          )
+          : (
+            <table className="main-table">
+              <thead className="main-table__header">
+                <tr
+                  className="main-table__header__row"
+                  key="header"
                 >
-                  <div className="main-table__checkbox">
-                    <CustomInput
-                      type="checkbox"
-                    />
-                  </div>
-                </th>
-              )}
-              {tableConfig.columns.map((column) => (
-                <th
-                  className={classNames({
-                    "main-table__head": true,
-                    [`main-table__head--${column.align}`]: column.align,
-                  })}
-                  style={{ width: column.width }}
-                  key={column.name}
-                >
-                  <div className="main-table__head-value">
-                    {
-                      onSort === false
-                        ? (
-                          column.description
-                        )
-                        : (
-                          <HeaderSort sortKey={column.name} onSort={onSort} activeSortKey={activeSortKey} isSortReverse={isSortReverse}>
-                            {column.description}
-                          </HeaderSort>
-                        )
-                    }
-                  </div>
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody className="main-table__body">
-            {visibleData.map((item) => (
-              <tr
-                className={classNames({
-                  "main-table__body__row": true,
-                  "main-table__body__row--selected": selectedData[item[tableConfig.idAttributeForData]],
-                })}
-                key={item[tableConfig.idAttributeForData]}
-              >
-                {tableConfig.checkbox && (
-                  <td
-                    className={classNames({
-                      "main-table__data": true,
-                      "main-table__data--center": true,
-                    })}
-                    style={{ width: "5%" }}
-                    key={"checkbox"}
-                  >
-                    <div className="main-table__checkbox">
-                      <CustomInput
-                        type="checkbox"
-                        checked={selectedData[item[tableConfig.idAttributeForData]]}
-                      />
-                    </div>
-                  </td>
-                )}
-                {tableConfig.columns.map((column) => (
-                  <td
-                    className={classNames({
-                      "main-table__data": true,
-                      [`main-table__data--${column.align}`]: column.align,
-                    })}
-                    key={column.name}
-                  >
-                    <div className={classNames({
-                      "main-table__data-value": true,
-                      "main-table__data--nowrap": !column.wrapText,
-                    })}>
-                      {item[column.data[0]]}
-                    </div>
-                    {column.data[1] && (
-                      <div className={classNames({
-                        "main-table__data-sub-value": true,
+                  {tableConfig.checkbox && (
+                    <th
+                      className={classNames({
+                        "main-table__head": true,
+                        "main-table__head--center": true,
                       })}
-                      >
-                        {item[column.data[1]]}
+                      style={{ width: tableConfig.checkboxWidth || "5%" }}
+                      key={"checkbox"}
+                    >
+                      <div className="main-table__checkbox">
+                        <CustomInput
+                          type="checkbox"
+                        />
                       </div>
+                    </th>
+                  )}
+                  {tableConfig.columns.map((column) => (
+                    <th
+                      className={classNames({
+                        "main-table__head": true,
+                        [`main-table__head--${column.align}`]: column.align,
+                      })}
+                      style={{ width: column.width }}
+                      key={column.name}
+                    >
+                      <div className="main-table__head-value">
+                        {
+                          onSort === false
+                            ? (
+                              column.description
+                            )
+                            : (
+                              <HeaderSort sortKey={column.name} onSort={onSort} activeSortKey={activeSortKey} isSortReverse={isSortReverse}>
+                                {column.description}
+                              </HeaderSort>
+                            )
+                        }
+                      </div>
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody className="main-table__body">
+                {visibleData.map((item) => (
+                  <tr
+                    className={classNames({
+                      "main-table__body__row": true,
+                      "main-table__body__row--selected": selectedData[item[tableConfig.idAttributeForData]],
+                    })}
+                    key={item[tableConfig.idAttributeForData]}
+                  >
+                    {tableConfig.checkbox && (
+                      <td
+                        className={classNames({
+                          "main-table__data": true,
+                          "main-table__data--center": true,
+                        })}
+                        style={{ width: "5%" }}
+                        key={"checkbox"}
+                      >
+                        <div className="main-table__checkbox">
+                          <CustomInput
+                            type="checkbox"
+                            checked={selectedData[item[tableConfig.idAttributeForData]]}
+                          />
+                        </div>
+                      </td>
                     )}
-                  </td>
+                    {tableConfig.columns.map((column) => (
+                      <td
+                        className={classNames({
+                          "main-table__data": true,
+                          [`main-table__data--${column.align}`]: column.align,
+                        })}
+                        key={column.name}
+                      >
+                        <div className={classNames({
+                          "main-table__data-value": true,
+                          "main-table__data--nowrap": !column.wrapText,
+                        })}>
+                          {item[column.data[0]]}
+                        </div>
+                        {column.data[1] && (
+                          <div className={classNames({
+                            "main-table__data-sub-value": true,
+                          })}
+                          >
+                            {item[column.data[1]]}
+                          </div>
+                        )}
+                      </td>
+                    ))}
+                  </tr>
                 ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+              </tbody>
+            </table>
+          )
+        }
       </div>
     );
   }
