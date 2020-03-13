5f410ccdd8a69a70309a410e181b9d97f0a040ad
Matheus Braga <matheus_ob59@hotmail.com>
2020-03-13 13:09:23 -0300

    History tab base simplistic template.

--- frontend/src/views/Dashboard/Dashboard.jsx
+++ frontend/src/views/Dashboard/Dashboard.jsx
@@ -11,35 +11,35 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <div className="flex-row align-items-center animated fadeIn">
-        <Container>
-          <Row className="justify-content-center">
-            <Col md="8">
-              <Card className="mx-4">
-                <CardBody className="p-4">
-                  <h3>
-                    Bem-vindo ao webSINFRA
-                    {" "}<i className="fa fa-wrench"></i>
-                  </h3>
-                  <br />
-                  <div className="text-muted text-justify">
-                    <p>
-                      Esta página dá acesso ao sistema de gestão de manutenção da
-                      Secretaria de Infraestrutura do Senado Federal - <b>webSINFRA</b>. Para uma melhor
-                      experiência, utilize a versão mais atual do navegador Chrome.
-                    </p>
-                    <p>
-                      Para reportar erros, sanar dúvidas ou sugerir melhorias,
-                      entre em contato com o SEPLAG no ramal 2339 ou envie um email para ls_seplag@senado.leg.br.
-                    </p>
-                  </div>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
+      // <div className="flex-row align-items-center animated fadeIn">
+      //   <Container>
+      //     <Row className="justify-content-center">
+      //       <Col md="8">
+      //         <Card className="mx-4">
+      //           <CardBody className="p-4">
+      //             <h3>
+      //               Bem-vindo ao webSINFRA
+      //               {" "}<i className="fa fa-wrench"></i>
+      //             </h3>
+      //             <br />
+      //             <div className="text-muted text-justify">
+      //               <p>
+      //                 Esta página dá acesso ao sistema de gestão de manutenção da
+      //                 Secretaria de Infraestrutura do Senado Federal - <b>webSINFRA</b>. Para uma melhor
+      //                 experiência, utilize a versão mais atual do navegador Chrome.
+      //               </p>
+      //               <p>
+      //                 Para reportar erros, sanar dúvidas ou sugerir melhorias,
+      //                 entre em contato com o SEPLAG no ramal 2339 ou envie um email para ls_seplag@senado.leg.br.
+      //               </p>
+      //             </div>
+      //           </CardBody>
+      //         </Card>
+      //       </Col>
+      //     </Row>
+      //   </Container>
 
-        {/* <Container>
+        /* <Container>
           <Row className="justify-content-center">
             <Col md="12">
               <Card className="mx-8">
@@ -57,8 +57,26 @@ class Dashboard extends Component {
               </Card>
             </Col>
           </Row>
-        </Container> */}
+        </Container> */
 
+      //</div>
+
+      <div>
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="8">
+              <Card className="mx-4">
+                <CardBody className="p-4">
+                  <div className="history__main">
+                    <div className="history__date">Quarta-feira 11/03/2020</div>
+                    <div className="history__occurence">Modificação no ativo.</div>
+                    <div className="history__time">15:22</div>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
