682ea0e5af7c644a84f2a0b842f64cc976b19c13
Matheus Braga <matheus_ob59@hotmail.com>
2020-03-16 12:09:06 -0300

    Improve history tab ui.

--- frontend/src/views/Dashboard/Dashboard.jsx
+++ frontend/src/views/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Container, Row, Col, Card, CardBody, } from "reactstrap";
 import { tableConfig, selectedData, data, searchableAttributes, filterAttributes, customFilters, dataTree } from './fakeData';
+import "./history.css";
 
 import CustomTable from "../../components/NewTables/CustomTable";
 
@@ -68,9 +69,15 @@ class Dashboard extends Component {
               <Card className="mx-4">
                 <CardBody className="p-4">
                   <div className="history__main">
-                    <div className="history__date">Quarta-feira 11/03/2020</div>
-                    <div className="history__occurence">Modificação no ativo.</div>
-                    <div className="history__time">15:22</div>
+                    <div className="history__date">11/03/2020 <span className="text-muted">Quarta-feira</span></div>
+                    <div className="history__items">
+                    <div className="history__icon"></div>
+                      <div className="history__occurence">
+                        <div className="history__creator">Nome</div>
+                        <div className="history__description">Modificação no ativo.</div>
+                        <div className="text-muted">15:22</div>
+                      </div>
+                    </div>
                   </div>
                 </CardBody>
               </Card>
