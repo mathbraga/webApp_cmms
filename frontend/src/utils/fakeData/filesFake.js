3165bcae13124b254ddddc53f02702a25c692abc
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-03-17 13:17:39 -0300

    Create fakeData for file's tab

---
+++ frontend/src/utils/fakeData/filesFake.js
@@ -0,0 +1,10 @@
+const files = [
+  {id: "0001", name: "Foto da estação transformadora - 000.jpg", format: "jpg", size: "6 MB", date: "10/03/2020", user: "Henrique Zaidan"},
+  {id: "0002", name: "Foto da estação transformadora - 001.jpg", format: "jpg", size: "6 MB", date: "11/03/2020", user: "Pedro Serafim"},
+  {id: "0003", name: "Foto da estação transformadora - 002.jpg", format: "jpg", size: "6 MB", date: "11/03/2020", user: "Matheus Braga"},
+  {id: "0004", name: "Relatório Mensal.pdf", format: "pdf", size: "2 MB", date: "13/03/2020", user: "Ricardo Palet"},
+  {id: "0005", name: "Memorando 010/2020 - COEMANT - Pagamento.docx", format: "docx", size: "4 MB", date: "15/03/2020", user: "Ricardo Palet"},
+  {id: "0006", name: "Planilha de custos.xlsx", format: "xlsx", size: "8 MB", date: "11/03/2020", user: "Matheus Braga"},
+];
+
+export default files;
