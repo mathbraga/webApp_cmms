57c4e07f55f4ad180e10998e74067c19df97864d
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-03-17 16:17:56 -0300

    Add different images for many file formats

--- frontend/src/components/Tables/RawTable/Body/FileIconElement.jsx
+++ frontend/src/components/Tables/RawTable/Body/FileIconElement.jsx
@@ -10,23 +10,36 @@ const propTypes = {
 };
 
 // Icons made by <a href="https://www.flaticon.com/authors/dimitry-miroliubov" title="Dimitry Miroliubov">Dimitry Miroliubov</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-const acceptedFileFormats = [
-  'pdf',
-  'doc', 'docx', 'txt',
-  'xls', 'xlsx', 'csv',
-  'ppt', 'pps', 'pptx',
-  'bmp', 'jpg', 'jpeg', 'gif', 'png', 'svg',
-  'msg', 'ost',
-  'zip', 'rar',
-  'dwg',
-  'py',
-];
+const iconFileFormats = {
+  'pdf': require('../../../../assets/img/files/pdf.png'),
+  'doc': require('../../../../assets/img/files/doc.png'), 
+  'docx': require('../../../../assets/img/files/docx.png'), 
+  'txt': require('../../../../assets/img/files/txt.png'),
+  'xls': require('../../../../assets/img/files/xls.png'), 
+  'xlsx': require('../../../../assets/img/files/xlsx.png'), 
+  'csv': require('../../../../assets/img/files/csv.png'),
+  'ppt': require('../../../../assets/img/files/ppt.png'), 
+  'pps': require('../../../../assets/img/files/pps.png'), 
+  'pptx': require('../../../../assets/img/files/pptx.png'),
+  'bmp': require('../../../../assets/img/files/bmp.png'), 
+  'jpg': require('../../../../assets/img/files/jpg.png'), 
+  'jpeg': require('../../../../assets/img/files/jpeg.png'), 
+  'gif': require('../../../../assets/img/files/gif.png'), 
+  'png': require('../../../../assets/img/files/png.png'), 
+  'svg': require('../../../../assets/img/files/svg.png'),
+  'msg': require('../../../../assets/img/files/msg.png'), 
+  'ost': require('../../../../assets/img/files/ost.png'),
+  'zip': require('../../../../assets/img/files/zip.png'), 
+  'rar': require('../../../../assets/img/files/rar.png'),
+  'py': require('../../../../assets/img/files/py.png'),
+  'unknown': require('../../../../assets/img/files/unknown.png'),
+}
 
 export default function FileIconElement({
   columnId,
   format
 }) {
-  const IconImage = require('../../../../assets/img/files/pdf.png');
+  const iconImage = iconFileFormats[format] || iconFileFormats['unknown'];
   return (
     <td
       className={classNames({
@@ -39,7 +52,7 @@ export default function FileIconElement({
       >
         <img
           style={{ width: "35px", height: "35px", cursor: "pointer" }}
-          src={IconImage}
+          src={iconImage}
         />
       </div>
     </td>
