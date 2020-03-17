1799139c04cad392feb40dd2e2e7b697acc13b5e
Matheus B <matheus_ob59@hotmail.com>
2020-03-17 13:09:16 -0300

    Realocate and improve history tab ui.

--- frontend/src/views/Dashboard/Dashboard.jsx
+++ frontend/src/views/Dashboard/Dashboard.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Container, Row, Col, Card, CardBody, } from "reactstrap";
 import { tableConfig, selectedData, data, searchableAttributes, filterAttributes, customFilters, dataTree } from './fakeData';
-import "./history.css";
 
 import CustomTable from "../../components/NewTables/CustomTable";
 
@@ -12,35 +11,35 @@ class Dashboard extends Component {
 
   render() {
     return (
-      // <div className="flex-row align-items-center animated fadeIn">
-      //   <Container>
-      //     <Row className="justify-content-center">
-      //       <Col md="8">
-      //         <Card className="mx-4">
-      //           <CardBody className="p-4">
-      //             <h3>
-      //               Bem-vindo ao webSINFRA
-      //               {" "}<i className="fa fa-wrench"></i>
-      //             </h3>
-      //             <br />
-      //             <div className="text-muted text-justify">
-      //               <p>
-      //                 Esta página dá acesso ao sistema de gestão de manutenção da
-      //                 Secretaria de Infraestrutura do Senado Federal - <b>webSINFRA</b>. Para uma melhor
-      //                 experiência, utilize a versão mais atual do navegador Chrome.
-      //               </p>
-      //               <p>
-      //                 Para reportar erros, sanar dúvidas ou sugerir melhorias,
-      //                 entre em contato com o SEPLAG no ramal 2339 ou envie um email para ls_seplag@senado.leg.br.
-      //               </p>
-      //             </div>
-      //           </CardBody>
-      //         </Card>
-      //       </Col>
-      //     </Row>
-      //   </Container>
+      <div className="flex-row align-items-center animated fadeIn">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="8">
+              <Card className="mx-4">
+                <CardBody className="p-4">
+                  <h3>
+                    Bem-vindo ao webSINFRA
+                    {" "}<i className="fa fa-wrench"></i>
+                  </h3>
+                  <br />
+                  <div className="text-muted text-justify">
+                    <p>
+                      Esta página dá acesso ao sistema de gestão de manutenção da
+                      Secretaria de Infraestrutura do Senado Federal - <b>webSINFRA</b>. Para uma melhor
+                      experiência, utilize a versão mais atual do navegador Chrome.
+                    </p>
+                    <p>
+                      Para reportar erros, sanar dúvidas ou sugerir melhorias,
+                      entre em contato com o SEPLAG no ramal 2339 ou envie um email para ls_seplag@senado.leg.br.
+                    </p>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
 
-        /* <Container>
+        {/* <Container>
           <Row className="justify-content-center">
             <Col md="12">
               <Card className="mx-8">
@@ -58,32 +57,8 @@ class Dashboard extends Component {
               </Card>
             </Col>
           </Row>
-        </Container> */
+        </Container> */}
 
-      //</div>
-
-      <div>
-        <Container>
-          <Row className="justify-content-center">
-            <Col md="8">
-              <Card className="mx-4">
-                <CardBody className="p-4">
-                  <div className="history__main">
-                    <div className="history__date">11/03/2020 <span className="text-muted">Quarta-feira</span></div>
-                    <div className="history__items">
-                    <div className="history__icon"><span>AB</span></div>
-                      <div className="history__occurence">
-                        <div className="history__creator">Nome</div>
-                        <div className="history__description">Modificação no ativo.</div>
-                        <div className="text-muted">15:22</div>
-                      </div>
-                    </div>
-                  </div>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
       </div>
     );
   }
--- frontend/src/views/Facility/TabsElements/LogTab.jsx
+++ frontend/src/views/Facility/TabsElements/LogTab.jsx
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
+import { Container, Row, Col, Card, CardBody, } from "reactstrap";
+import "./history.css";
 
 class LogTab extends Component {
   state = {}
   render() {
-    return (<h1>Hiihih!</h1>);
+    return (
+    <div>
+      {/* <Container> */}
+        {/* <Row className="justify-content-center"> */}
+          {/* <Col md="8">
+            <Card className="mx-4">
+              <CardBody className="p-4"> */}
+                <div className="history__main">
+                  <div className="history__date">11/03/2020 <span className="text-muted">Quarta-feira</span></div>
+                  <div className="history__items">
+                    <div className="history__icon"></div>
+                    <div className="history__occurence">
+                      <div className="history__creator">Nome</div>
+                      <div className="history__description">atualizou o ativo <span>CASF</span></div>
+                      <div className="history__time text-muted">15:22</div>
+                      <ul>
+                        <li>Nome do ativo alterado para "Novo nome"</li>
+                        <li>Valor do ativo alterado para "Novo valor"</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="history__items">
+                    <div className="history__icon"></div>
+                    <div className="history__occurence">
+                      <div className="history__creator">Nome</div>
+                      <div className="history__description">atualizou o ativo <span>CASF</span></div>
+                      <div className="history__time text-muted">15:22</div>
+                      <ul>
+                        <li>Nome do ativo alterado para "Novo nome"</li>
+                        <li>Valor do ativo alterado para "Novo valor"</li>
+                        <li>A</li>
+                        <li>A</li>
+                        <li>A</li>
+                        <li>A</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              {/* </CardBody>
+            </Card>
+          </Col> */}
+        {/* </Row> */}
+      {/* </Container> */}
+    </div>
+    );
   }
 }
 
