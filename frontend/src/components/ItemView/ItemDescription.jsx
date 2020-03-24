d5a9aa0d581d24d4f9e15dda6ba9f90e4dc55d77
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-03-24 16:45:21 -0300

    Refactor and fix problems on ItemView.css

--- frontend/src/components/ItemView/ItemDescription.jsx
+++ frontend/src/components/ItemView/ItemDescription.jsx
@@ -15,40 +15,38 @@ export default class ItemDescription extends Component {
   render() {
     const { image, status, descriptionItems } = this.props;
     return (
-      <div className="item-description">
-        <Row>
-          <Col md="2" style={{ textAlign: "left" }}>
-            <div className="desc-box">
-              <div className="desc-img-container">
-                <img className="desc-image" src={image} alt="Ar-condicionado" />
-              </div>
-              <div className="desc-status">
-                <Badge className="mr-1 desc-badge" color="success">{status}</Badge>
-              </div>
-            </div>
-          </Col>
-          <Col className="flex-column" md="10">
-            {descriptionItems.map((item, index) => {
-              if (index === 0) {
-                return (
-                  <div style={{ flexGrow: "1" }} key={index}>
-                    <Row>
-                      <Col md="3" style={{ textAlign: "end" }}><span className="desc-name">{item.title}</span></Col>
-                      <Col md="9" style={{ textAlign: "justify", paddingTop: "5px" }}><span>{item.description || "Não cadastrado"}</span></Col>
-                    </Row>
-                  </div>
-                );
-              }
-
+      <div className="description-container">
+        <div className="description-container__status-box">
+          <div className="status-box__image-container">
+            <img className="status-box__image" src={image} alt="Ar-condicionado" />
+          </div>
+          <div className="status-box__badge-box">
+            <Badge className="mr-1 status-box__badge" color="success">{status}</Badge>
+          </div>
+        </div>
+        <div className="description-container__text-box" md="10">
+          {descriptionItems.map((item, index) => {
+            if (index === 0) {
               return (
-                <Row key={index}>
-                  <Col md="3" style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}><span className="desc-sub">{item.title}</span></Col>
-                  <Col md="9" style={{ display: "flex", alignItems: "center" }}><span>{item.description || "Não cadastrado"}</span></Col>
-                </Row>
+                <div className="text-box__first-line" key={index}>
+                  <Row>
+                    <Col md="3" className="text-box__lines__col-title"><span className="text-box__title">{item.title}</span></Col>
+                    <Col md="9" className="text-box__lines__col-value"><span>{item.description || "Não cadastrado"}</span></Col>
+                  </Row>
+                </div>
               );
-            })}
-          </Col>
-        </Row>
+            }
+
+            return (
+              <div className="text-box__lines" key={index}>
+                <Row>
+                  <Col md="3" className="text-box__lines__col-title"><span className="text-box__subtitle">{item.title}</span></Col>
+                  <Col md="9" className="text-box__lines__col-value"><span>{item.description || "Não cadastrado"}</span></Col>
+                </Row>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
