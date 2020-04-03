d50eae43a07b046c4375f2c501c0fb565fa8cda3
hzlopes <hzlopes@gmail.com>
2020-04-03 17:42:21 -0300

    File for adding extra indexes

--- database/_script.sql
+++ database/_script.sql
@@ -96,6 +96,9 @@ begin transaction;
 -- commit transaction
 commit transaction;
 
+-- create extra indexes
+-- \i end-indexes.sql
+
 -- set the default transaction isolation level
 alter database :new_db_name set default_transaction_isolation to 'serializable';
 
