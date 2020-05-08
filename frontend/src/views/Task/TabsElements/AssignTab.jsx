3e72baf99be2fe2ad835989c470c60adf7835d5d
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-05-08 16:47:32 -0300

    Add descriptionTable on AssignTab

--- frontend/src/views/Task/TabsElements/AssignTab.jsx
+++ frontend/src/views/Task/TabsElements/AssignTab.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
+import DescriptionTable from '../../../components/Descriptions/DescriptionTable';
+import CustomTable from '../../../components/Tables/CustomTable';
+
 class AssignTab extends Component {
   state = {}
   render() {
+    const data = this.props.data.assets;
     return (
-      <h1>HI!!!!</h1>
+      <>
+        <DescriptionTable
+          title={'Lista de Ativos'}
+          numColumns={2}
+          itemsMatrix={itemsMatrixAssets(data)}
+        />
+        {/* <CustomTable
+          type={'pages-with-search'}
+          tableConfig={tableConfig}
+          customFilters={customFilters}
+          filterAttributes={filterAttributes}
+          searchableAttributes={searchableAttributes}
+          selectedData={this.props.selectedData}
+          handleSelectData={this.props.handleSelectData}
+          data={data}
+        /> */}
+      </>
     );
   }
 }
