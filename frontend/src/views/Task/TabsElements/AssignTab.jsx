75c2e9e1820d485c8b58620bb0f1ae1acdfd57ba
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-05-11 18:25:35 -0300

    Create small form for dispatches

--- frontend/src/views/Task/TabsElements/AssignTab.jsx
+++ frontend/src/views/Task/TabsElements/AssignTab.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 import DescriptionTable from '../../../components/Descriptions/DescriptionTable';
 import CustomTable from '../../../components/Tables/CustomTable';
-import { Button } from 'reactstrap';
+import { Button, Input } from 'reactstrap';
+import Select from 'react-select';
 import { itemsMatrixAssetsHierachy } from '../utils/dispatchTab/descriptionMatrix';
 
 class AssignTab extends Component {
@@ -14,13 +15,50 @@ class AssignTab extends Component {
         <div 
           className='action-container'
         >
-          <div className="action-text">Tramitar Tarefa / Alterar Status</div>
-          <div className='action-buttons'>
+          <div className="action__text">Tramitar Tarefa / Alterar Status</div>
+          <div className='action__buttons'>
             <Button color="success" size="sm" style={{ marginRight: "10px" }}>
               Tramitar
             </Button>
             <Button color="primary" size="sm">
               Alterar Status
+            </Button>
+          </div>
+        </div>
+        <div className='miniform-container'>
+          <div className='miniform__field'>
+            <div className='miniform__field__label'>
+              Tramitar para
+            </div>
+            <div className='miniform__field__input'>
+              <Select
+                className="basic-single"
+                classNamePrefix="select"
+                defaultValue={'Semac'}
+                isClearable
+                isSearchable
+                name="team"
+                options={[{value: 'Semac', label: 'Semac'}, {value: 'Coemant', label: 'Coemant'}, {value: 'Sinfra', label: 'Sinfra'}]}
+              />
+            </div>
+          </div>
+          <div className='miniform__field'>
+            <div className='miniform__field__label'>
+              Justificativa / Observações
+            </div>
+            <div className='miniform__field__input'>
+              <Input style={{ width: "100%" }} type="textarea" name="text" id="exampleText" />
+            </div>
+          </div>
+          <div className='miniform__buttons'>
+            <Button color="success" size="sm" style={{ marginRight: "10px" }}>
+              Tramitar
+            </Button>
+            <Button color="secondary" size="sm" style={{ marginRight: "10px" }}>
+              Limpar
+            </Button>
+            <Button color="danger" size="sm">
+              Cancelar
             </Button>
           </div>
         </div>
