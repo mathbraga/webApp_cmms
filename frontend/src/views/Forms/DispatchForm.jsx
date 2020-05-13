66c329bd122cc31797a55874e6cf4ee56038c868
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-05-13 18:49:19 -0300

    Create new component for DispatchForm

---
+++ frontend/src/views/Forms/DispatchForm.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class DispatchForm extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <h1>Hey</h1>
+     );
+  }
+}
+ 
+export default DispatchForm;
