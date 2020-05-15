005fdd7fbf32b82b186aaf8501646c02752091e8
hzlopes <hzlopes@gmail.com>
2020-05-15 13:18:17 -0300

    Fix script, rename files

--- database/next/_next.sql
+++ database/next/_next.sql
@@ -56,7 +56,7 @@ begin transaction;
 \i prep/types.sql
 
 -- create domains
--- \i prep-domains.sql
+-- \i prep/domains.sql
 
 -- create tables
 \i prep/lookup_tables.sql
@@ -66,13 +66,10 @@ begin transaction;
 \i prep/get_asset_trees.sql
 \i prep/get_exception_message.sql
 
--- triggers
--- \i trigger/name_of_the_trigger.sql
-
 -- create api
 -- task
 \i api/task/insert_task.sql
-\i api/task_data.sql
+\i api/task/task_data.sql
 \i api/task/form_data.sql
 -- asset
 \i api/asset/appliance_data.sql
@@ -83,25 +80,28 @@ begin transaction;
 -- spec
 \i api/spec/spec_data.sql
 -- team
-\i api/team/
+\i api/team/team_data.sql
 
--- -- others
--- \i end-uuid.sql
+-- others
+-- \i end/uuid.sql
 
--- -- create and login with fake user for initial inserts
--- \i end-fake-user.sql
+-- create and login with fake user for initial inserts
+-- \i end/fake_user.sql
 
--- -- populate tables
--- \i end-populate-tables.sql
+-- create triggers before populate tables
+-- \i trigger/name_of_the_trigger.sql
 
--- -- create triggers
--- \i end-triggers.sql
+-- populate tables
+-- \i end/populate_tables.sql
 
--- -- create rls policies
--- -- \i end-rls-policies.sql
+-- create triggers after populate tables
+-- \i tigger/name_of_the_trigger.sql
 
--- -- restart sequences
--- \i end-restart-sequences.sql
+-- create rls policies
+-- \i end/rls_policies.sql
+
+-- restart sequences
+-- \i end/restart_sequences.sql
 
 -- set ON_ERROR_STOP to off
 \set ON_ERROR_STOP off
@@ -110,7 +110,7 @@ begin transaction;
 commit transaction;
 
 -- create extra indexes
--- \i end-indexes.sql
+-- \i end/indexes.sql
 
 -- set the default transaction isolation level
 -- alter database :new_db_name set default_transaction_isolation to 'serializable';
