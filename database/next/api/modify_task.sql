c09f507d8befc672745aba8660b051ae9a7b6b09
hzlopes <hzlopes@gmail.com>
2020-05-15 17:05:39 -0300

    Add old code of modify_task function

--- database/next/api/modify_task.sql
+++ database/next/api/modify_task.sql
@@ -1,2 +1,65 @@
 drop function if exists api.modify_task;
 
+create or replace function api.modify_task (
+  inout id integer,
+  in attributes tasks,
+  in assets integer[]
+)
+  language plpgsql
+  strict
+  as $$
+    begin
+      update tasks as t
+        set (
+          task_status_id,
+          task_priority_id,
+          task_category_id,
+          project_id,
+          team_id,
+          title,
+          description,
+          -- todo: request fields
+          place,
+          progress,
+          date_limit,
+          date_start,
+          updated_at
+        ) = (
+          attributes.task_status_id,
+          attributes.task_priority_id,
+          attributes.task_category_id,
+          attributes.project_id,
+          attributes.team_id,
+          attributes.title,
+          attributes.description,
+          -- todo: request fields
+          attributes.place,
+          attributes.progress,
+          attributes.date_limit,
+          attributes.date_start,
+          default
+        ) where t.task_id = id;
+
+      with added_assets as (
+        select unnest(assets) as asset_id
+        except
+        select asset_id
+          from task_assets as ta
+        where ta.task_id = id
+      )
+      insert into task_assets
+        select id, asset_id from added_assets;
+
+      with recursive removed_assets as (
+        select asset_id
+          from task_assets as ta
+        where ta.task_id = id
+        except
+        select unnest(assets) as asset_id
+      )
+      delete from task_assets as ta
+        where ta.task_id = id
+              and asset_id in (select asset_id from removed_assets);
+    end;
+  $$
+;
