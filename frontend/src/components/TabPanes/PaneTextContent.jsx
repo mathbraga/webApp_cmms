428b51e7478435f87d26b6ea7338f36e37d161b5
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-05-15 18:52:47 -0300

    Adjust logic for PaneTextContent

--- frontend/src/components/TabPanes/PaneTextContent.jsx
+++ frontend/src/components/TabPanes/PaneTextContent.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import TextField from './TextField';
+import { Row, Col } from 'reactstrap';
 
-class PaneTextContent extends Component {
-  state = {  }
-  render() { 
-    return (  );
+export default class PaneTextContent extends Component {
+  render() {
+    const { numColumns, itemsMatrix } = this.props;
+    return (
+      <div className="asset-info-container">
+        {itemsMatrix && (
+          <div className="asset-info-content">
+            {
+              itemsMatrix.map((line, index) => (
+                <Row
+                  key={index}
+                >
+                  {
+                    line && line.map((item) => (
+                      <Col md={(12 / numColumns * item.span).toString()} style={item.style} key={item.id}>
+                        {item.elementGenerator ?
+                          item.elementGenerator() :
+                          <TextField
+                            title={item.title}
+                            description={item.description}
+                          />
+                        }
+                      </Col>
+                    ))
+                  }
+                </Row>
+              ))
+            }
+          </div>
+        )}
+      </div>
+    );
   }
 }
- 
-export default PaneTextContent;
