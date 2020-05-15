2cdf379f1833f528340ce2bdd58dacdfac980473
Matheus B <matheus_ob59@hotmail.com>
2020-05-15 16:55:24 -0300

    Create login page for redirect.

--- backend/middlewares/redirect-unauth.js
+++ backend/middlewares/redirect-unauth.js
@@ -1,7 +1,8 @@
+const path = require('path');
+
 const redirectUnauth = (req, res, next) => {
   if (!req.user) {
-    // res.status(401).end();
-    next();
+    res.sendFile(path.join(__dirname, '../public/html/login.html'))
   } else {
     next();
   }
