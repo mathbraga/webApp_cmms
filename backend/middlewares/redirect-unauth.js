739196be582b78e1abe9dbdb76519524d1483e7c
Matheus B <matheus_ob59@hotmail.com>
2020-05-19 15:06:20 -0300

    Add CSS to login page.

--- backend/middlewares/redirect-unauth.js
+++ backend/middlewares/redirect-unauth.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const redirectUnauth = (req, res, next) => {
   if (!req.user) {
-    res.sendFile(path.join(__dirname, '../public/html/login.html'))
+    res.sendFile(path.join(__dirname, '../public/login/login.html'))
   } else {
     next();
   }
