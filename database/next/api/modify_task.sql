792a38952cb9b3847b095c1ae84c31eb03b5f0a0
hzlopes <hzlopes@gmail.com>
2020-05-21 20:07:14 -0300

    Remove old tasks table structure

--- database/next/api/modify_task.sql
+++ database/next/api/modify_task.sql
@@ -11,33 +11,9 @@ create or replace function api.modify_task (
     begin
       update tasks as t
         set (
-          task_status_id,
-          task_priority_id,
-          task_category_id,
-          project_id,
-          team_id,
-          title,
-          description,
-          -- todo: request fields
-          place,
-          progress,
-          date_limit,
-          date_start,
-          updated_at
+          
         ) = (
-          attributes.task_status_id,
-          attributes.task_priority_id,
-          attributes.task_category_id,
-          attributes.project_id,
-          attributes.team_id,
-          attributes.title,
-          attributes.description,
-          -- todo: request fields
-          attributes.place,
-          attributes.progress,
-          attributes.date_limit,
-          attributes.date_start,
-          default
+          
         ) where t.task_id = id;
 
       with added_assets as (
@@ -58,8 +34,8 @@ create or replace function api.modify_task (
         select unnest(assets) as asset_id
       )
       delete from task_assets as ta
-        where ta.task_id = id
-              and asset_id in (select asset_id from removed_assets);
+        where ta.task_id = id and
+              asset_id in (select asset_id from removed_assets);
     end;
   $$
 ;
