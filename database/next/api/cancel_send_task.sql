fe78091139822a67b8247174a4417468ab6f13da
hzlopes <hzlopes@gmail.com>
2020-05-21 18:38:22 -0300

    Cancel send task

--- database/next/api/cancel_send_task.sql
+++ database/next/api/cancel_send_task.sql
@@ -1,6 +1,6 @@
-drop function if exists api.receive_task;
+drop function if exists api.cancel_send_task;
 
-create or replace function api.receive_task (
+create or replace function api.cancel_send_task (
   in attributes task_events,
   out id integer
 )
@@ -8,17 +8,15 @@ create or replace function api.receive_task (
   as $$
     begin
 
-      update tasks set (team_id) = (attributes.team_id);
-
       insert into task_events values (
         attributes.task_id,
-        'receive'::task_event_enum,
+        'cancel'::task_event_enum,
         now(),
         get_current_person_id(),
         attributes.team_id,
         null,
-        attributes.task_status_id,
         null,
+        null
       ) returning task_id into id;
 
     end;
