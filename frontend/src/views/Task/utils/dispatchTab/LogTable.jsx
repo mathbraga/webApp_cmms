a2c925d886e204411eac64df589d791632311ed4
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-05-21 17:27:45 -0300

    Create LogTable file

---
+++ frontend/src/views/Task/utils/dispatchTab/LogTable.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class LogTable extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <h1>Hey</h1>
+     );
+  }
+}
+ 
+export default LogTable;
