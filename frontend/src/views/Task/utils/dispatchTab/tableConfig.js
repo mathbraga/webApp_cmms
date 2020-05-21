456e2ba3f3fe4e7f9f3acdb452a2708de83d30a6
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-05-21 17:26:23 -0300

    Add prepareData on tableConfig

--- frontend/src/views/Task/utils/dispatchTab/tableConfig.js
+++ frontend/src/views/Task/utils/dispatchTab/tableConfig.js
@@ -1,8 +1,24 @@
+function generateEventName(item) {
+  const relationEvents = {
+    insert: 'Criado por',
+    send: 'Tramitado para',
+    receive: 'Tramitação recebida',
+    cancel: 'Tramitação cancelada',
+    move: 'Status alterado'
+  }
+  return relationEvents[item.event];
+}
+
+
+
 const tableConfig = {
   attForDataId: 'time',
+  prepareData: {
+    eventName: generateEventName,
+  },
   columnsConfig: [
     { columnId: 'date', columnName: 'Data', width: '15%', align: "center", idForValues: ['time']},
-    { columnId: 'event', columnName: 'Evento', width: '20%', align: "justify", idForValues: ['event']},
+    { columnId: 'event', columnName: 'Evento', width: '20%', align: "justify", idForValues: ['eventName']},
     { columnId: 'team', columnName: 'Equipe', width: '25%', align: "justify", idForValues: ['senderName', 'personName']},
     { columnId: 'note', columnName: 'Observação', width: '40%', align: "justify", isTextWrapped: true, idForValues: ['note']},
   ]
