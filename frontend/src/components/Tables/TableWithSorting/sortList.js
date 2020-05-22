1f1d6dd5283778fee44ae51f65d4a5040c2190c5
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-05-22 16:46:07 -0300

    Refactor function sortList

---
+++ frontend/src/components/Tables/TableWithSorting/sortList.js
@@ -0,0 +1,20 @@
+export default function sortList(data, sortKey, isSortReverse, isDataTree) {
+  if (isDataTree) {
+    let sortedList = {};
+    if (sortKey) {
+      Object.keys(data).forEach((parent) => sortedList[parent] = sortBy(data[parent], sortKey));
+      if (isSortReverse) {
+        Object.keys(data).forEach((parent) => sortedList[parent].reverse());
+      }
+    } else {
+      sortedList = data;
+    }
+    return sortedList;
+  }
+
+  // It is not DataTree
+  const sortedList = sortKey ? sortBy(data, sortKey) : data;
+  const reverseSortedList = (isSortReverse && sortKey) ? sortedList.reverse() : sortedList;
+
+  return reverseSortedList;
+}
