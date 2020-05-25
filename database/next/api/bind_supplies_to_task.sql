5a7806449b220417f9f9ebc3eafa3dd5e995b590
hzlopes <hzlopes@gmail.com>
2020-05-25 17:15:56 -0300

    Create function bind_supplies_to_task

--- database/next/api/bind_supplies_to_task.sql
+++ database/next/api/bind_supplies_to_task.sql
@@ -3,23 +3,19 @@ drop function if exists api.bind_supplies_to_task;
 create or replace function api.bind_supplies_to_task (
   inout id integer,
   in supplies integer[],
-  in quantities integer[]
+  in quantities numeric[]
 )
   language plpgsql
   as $$
     begin
-      insert into task_supplies as ts
-        values (
-          id,
-          unnest(supplies),
-          unnest(quantities)
-        )
-        on conflict on constraint task_supplies_pkey
-        do update set (
-          qty
-        ) = (
-          
-        );
+      -- remove old supplies
+      delete from task_supplies where task_id = id;
+      -- insert new supplies
+      insert into task_supplies
+        select  id,
+                unnest(supplies),
+                unnest(quantities)
+      ;
     end;
   $$
 ;
