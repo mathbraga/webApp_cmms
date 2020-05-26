ac9899ac28fef6f3b09912da8d5a302332ec2846
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-05-26 17:41:41 -0300

    Create descriptionMatrix on tab supply

---
+++ frontend/src/views/Task/utils/materialTab/descriptionMatrix.js
@@ -0,0 +1,13 @@
+export function itemsMatrixSupply(data) {
+  return (
+    [
+      [
+        { id: 'taskValue', title: 'Valor da tarefa', description: 'R$ 1.341,00', span: 1 },
+      ],
+      [
+        { id: 'team', title: 'Gasto com material', description: 'R$ 641,00', span: 1 },
+        { id: 'totalDays', title: 'Gasto com serviço', description: 'R$ 700,00', span: 1 }
+      ],
+    ]
+  );
+}
