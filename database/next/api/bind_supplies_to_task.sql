3dfbdabb8228e8afde8162480dba3bfb3bfb4bd3
hzlopes <hzlopes@gmail.com>
2020-05-26 16:49:03 -0300

    Add jsonb_to_recordset

--- database/next/api/bind_supplies_to_task.sql
+++ database/next/api/bind_supplies_to_task.sql
@@ -4,6 +4,7 @@ create or replace function api.bind_supplies_to_task (
   inout id integer,
   in supplies integer[],
   in quantities numeric[]
+  -- in jsupplies jsonb
 )
   language plpgsql
   as $$
@@ -15,6 +16,17 @@ create or replace function api.bind_supplies_to_task (
         select  id,
                 unnest(supplies),
                 unnest(quantities)
+
+        -- OR
+        -- select  id,
+        --         x."supplyId", -- mandatory double quotes because of camelCase key
+        --         x."qty"
+        --   from jsonb_to_recordset(
+        --     jsupplies
+        --   ) as x(
+        --     "supplyId" integer,
+        --     "qty" numeric
+        --   )
       ;
     end;
   $$
