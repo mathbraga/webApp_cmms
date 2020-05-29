2664285799b3678136620960aa13d8a9480f05b1
hzlopes <hzlopes@gmail.com>
2020-05-29 13:03:34 -0300

    Update create index commands

--- database/next/public/indexes.sql
+++ database/next/public/indexes.sql
@@ -1,9 +1,8 @@
 create index on asset_relations (parent_id);
 create index on task_files (task_id);
 create index on task_messages (task_id);
-create index on task_dispatch_logs (task_id);
-create index on task_status_logs (task_id);
+create index on task_events (task_id);
 -- Ideas for more indexes:
--- table: supplies / column: spec_id
--- table: task_supplies / column: supply_id
--- table: task_assets / column: asset_id
+-- create index on supplies (spec_id);
+-- create index on task_supplies (supply_id);
+-- create index on task_assets (asset_id);
