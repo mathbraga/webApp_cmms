fad13ef9d4c47e64216f08479ab50a303cc7b40e
Matheus B <matheus_ob59@hotmail.com>
2020-06-01 22:46:19 -0300

    Uncomment wrongly commented line of code.

--- backend/middlewares/redirect-unauth.js
+++ backend/middlewares/redirect-unauth.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 const redirectUnauth = (req, res, next) => {
   if (!req.user) {
-    // res.sendFile(path.join(__dirname, '../public/login/login.html'));
-    next();
+    res.sendFile(path.join(__dirname, '../public/login/login.html'));
+    // next();
   } else {
     // res.send('User connected.');
     next();
