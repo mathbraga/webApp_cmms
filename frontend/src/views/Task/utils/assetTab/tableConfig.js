6eb1c71293dd3a3bf47768bc914990c8cbb23664
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-06-03 15:57:53 -0300

    Adjust asset tableConfig

--- frontend/src/views/Task/utils/assetTab/tableConfig.js
+++ frontend/src/views/Task/utils/assetTab/tableConfig.js
@@ -3,13 +3,14 @@ import React from 'react';
 const mapIcon = require("../../../../assets/icons/map.png");
 
 const tableConfig = {
-  attForDataId: 'time',
+  attForDataId: 'assetId',
+  isItemClickable: false,
+  dataAttForClickable: 'title',
   columnsConfig: [
-    { columnId: 'date', columnName: 'Data', width: '15%', align: "center", idForValues: ['time']},
-    { columnId: 'event', columnName: 'Evento', width: '20%', align: "justify", idForValues: ['event']},
-    { columnId: 'team', columnName: 'Equipe', width: '25%', align: "justify", idForValues: ['senderName', 'personName']},
-    { columnId: 'note', columnName: 'Observação', width: '40%', align: "justify", isTextWrapped: true, idForValues: ['note']},
-  ]
+    { columnId: 'context', columnName: 'Contexto', width: "25%", align: "center", idForValues: ['categoryName'] },
+    { columnId: 'place', columnName: 'Ativo', width: "60%", align: "justify", idForValues: ['name', 'assetSf'] },
+    { columnId: 'map', columnName: 'Planta', width: "15%", align: "center", createElement: (<img src={mapIcon} alt="Google Maps" style={{ width: "35px", height: "35px" }} />) }
+  ],
 };
 
 export default tableConfig;
