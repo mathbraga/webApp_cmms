5e8888870b0175440b07c148ba08a267b26de114
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-06-05 15:44:31 -0300

    Add PaneTitle on message tab

--- frontend/src/views/Task/TabsElements/MessageTab.jsx
+++ frontend/src/views/Task/TabsElements/MessageTab.jsx
@@ -3,7 +3,27 @@ import React, { Component } from 'react';
 class MessageTab extends Component {
   render() {
     return (
-      <h1>HI!!!!</h1>
+      <div className="tabpane-container">
+        <PaneTitle 
+          actionButtons={editFormOpen ? actionButtons.editFormOpen : actionButtons.noFormOpen}
+          title={editFormOpen ? 'Alterar ativos' : 'Tabela de ativos'}
+        />
+        <AnimateHeight 
+          duration={300}
+          height={heightEdit}
+        >
+          <div className="tabpane__content">
+            <EditAssetForm 
+              toggleForm={this.toggleEditForm}
+            />
+          </div>
+        </AnimateHeight>
+        {(editFormOpen) && (
+          <PaneTitle 
+            title={'Tabela de Ativos'}
+          />
+        )}
+      </div>
     );
   }
 }
