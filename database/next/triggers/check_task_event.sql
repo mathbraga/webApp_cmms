ab72c2cc457485fe39f1f643e9945a141f99a2ed
hzlopes <hzlopes@gmail.com>
2020-06-08 15:34:29 -0300

    Refactor check task event trigger

--- database/next/triggers/check_task_event.sql
+++ database/next/triggers/check_task_event.sql
@@ -6,33 +6,39 @@ create or replace function check_task_event ()
   language plpgsql
   as $$
     declare
-      last_event_time timestamptz;
       is_event_ok boolean;
     begin
 
-      with last_event as (
-        select  te.task_id,
-                max(te.event_time) into last_event_time
-            from task_events as te
-          where te.task_id = new.task_id
-        group by te.task_id
+      with last_send as (
+        select  t.task_id,
+                t.team_id,
+                t.next_team_id
+          from tasks as t
+        where t.task_id = new.task_id
       )
       select  case new.event_name
-                -- when 'insert' then true
-                when 'send' then new.next_team_id is not null
-                when 'receive' then new.team_id = te.next_team_id
-                when 'cancel' then new.team_id = te.team_id
+                when 'insert' then true
+                when 'send' then (
+                  new.next_team_id is not null and
+                  (
+                    ls.team_id = new.team_id or
+                    current_role = 'administrator' or
+                    current_role = 'supervisor'
+                  )
+                )
+                when 'receive' then (
+                  new.team_id = ls.next_team_id and
+                  new.task_status_id is not null
+                )
+                when 'cancel' then new.team_id = ls.team_id
                 when 'move' then new.task_status_id is not null
               end as is_event_ok
-          from task_events as te
-        where te.task_id = new.task_id and
-            te.event_time = last_event_time
+        from last_send as ls
       ;
 
-      if
-        is_event_ok then return new;
-        else
-        raise exception '%', get_exception_message(8);
+      if is_event_ok
+        then return new;
+        else raise exception '%', get_exception_message(8);
       end if;
 
     end;
