bba61117e75c201f6de657cceab366c166e10fee
hzlopes <hzlopes@gmail.com>
2020-06-19 14:09:53 -0300

    Add psql command line

--- database/debug.sql
+++ database/debug.sql
@@ -1,4 +1,7 @@
 -- check connections
 select * from pg_stat_activity where datname = 'cmms';
 
+-- command line to execute test.sql file on dbname database
+psql -d dbname -f test.sql
+
 -- other commands to help debugging:
