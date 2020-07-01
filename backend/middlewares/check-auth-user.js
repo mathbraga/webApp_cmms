bbac035e70526afae9ae87fdc9c127efd4eed683
Matheus B <matheus_ob59@hotmail.com>
2020-07-01 19:06:37 -0300

    Remove express cookie related code.

--- backend/middlewares/check-auth-user.js
+++ backend/middlewares/check-auth-user.js
@@ -3,10 +3,12 @@ const checkAuthUser = (req, res, next) => {
     // res.sendFile has to be commented for db connection to work in frontend testing
     // res.sendFile(path.join(__dirname, '../public/login/login.html'));
     // res.redirect('/login.html');
+    console.log('no user')
     next();
   } else {
     // res.send('User connected.');
     // res.json(req.cookies);
+    console.log(req.cookies);
     next();
   }
 }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,9 +8,9 @@ router.post(
   (req, res) => {
     if(req.user){
       // console.log(req.user)
-      let cookieContent = req.user.personId.toString() + '-' + req.user.role;
+      // let cookieContent = req.user.personId.toString() + '-' + req.user.role;
       // console.log(cookieContent)
-      res.cookie('cmms:user', cookieContent);
+      // res.cookie('cmms:user', cookieContent);
       res.json(req.user);
     }
   }
@@ -21,7 +21,6 @@ router.get(
   (req, res) => {
     req.logout();
     req.session = null;
-    res.clearCookie('cmms:user');
     res.json({ logoutSuccess: true });
   }
 );
