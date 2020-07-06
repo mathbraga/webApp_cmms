7d2c9b48a679369b267f760b65fe89a7f4764833
Matheus B <matheus_ob59@hotmail.com>
2020-07-06 19:19:35 -0300

    Fix error 404 on authcookie route.

--- backend/routes/cookieAuth.js
+++ backend/routes/cookieAuth.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const paths = require('../paths');
 
 router.get(
-  paths.cookieAuth,
+  '/',
   (req, res) => {
     if (req.user) {
       res.json(req.cookies);
