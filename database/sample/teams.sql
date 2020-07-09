99fd0e6fca4673c4aac31713d86489298cfdc855
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-07-09 15:55:01 -0300

    Add more samples on Teams and Users

--- database/sample/teams.sql
+++ database/sample/teams.sql
@@ -1,4 +1,7 @@
-insert into teams overriding system value values (1, 'SEPLAG', 'Equipe do SEPLAG', true);
-insert into teams overriding system value values (2, 'SEMAC', 'Colaboradores do SEMAC', true);
-insert into teams overriding system value values (3, 'Posto 1 - Contrato', 'Atendimento de demandas do Anexo 2', true);
-insert into teams overriding system value values (4, 'Manutenção civil', 'Todos envolvidos na fiscalização do contrato', true);
+insert into teams overriding system value values (1, 'Sinfra', 'Colaboradores do SEMAC', true);
+insert into teams overriding system value values (2, 'Coemant', 'Equipe do SEPLAG', true);
+insert into teams overriding system value values (3, 'Seplag', 'Equipe do SEPLAG', true);
+insert into teams overriding system value values (4, 'Semac', 'Colaboradores do SEMAC', true);
+insert into teams overriding system value values (5, 'RCS Tecnologia - Posto 01', 'Atendimento de demandas do Anexo 2', true);
+insert into teams overriding system value values (6, 'RCS Tecnologia - Administração', 'Todos envolvidos na fiscalização do contrato', true);
+insert into teams overriding system value values (7, 'Entherm - Posto 01', 'Atendimento de demandas do Anexo 2', true);
