11a3e157541758c7a30df4d348812bb6d230d2b6
hzlopes <hzlopes@gmail.com>
2020-07-09 12:17:32 -0300

    Fix paths and gitignore

--- backend/paths.js
+++ backend/paths.js
@@ -1,15 +1,20 @@
 const paths = {
+
+  // Routes paths
   api: '/api',
   login: '/login',
   logout: '/logout',
   download: '/download',
   upload: '/api', // Must be the same as api path
   graphiql: '/graphiql',
+
+  // Directories paths
   public: '/public',
   images: '/public/images',
-  files: '/public/files',
+  files: '/files',
   httpLog: '/logs/http.log',
   cronLog: '/logs/cron.log',
+
 };
 
 module.exports = paths;
