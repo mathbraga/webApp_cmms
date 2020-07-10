25f4c0b4c591faeb3253c2ff67af000d72225fd0
hzlopes <hzlopes@gmail.com>
2020-07-10 10:58:29 -0300

    Reorganize, rename tests

--- backend/tests/routes/download.test.js
+++ backend/tests/routes/download.test.js
@@ -2,9 +2,10 @@ const path = require('path');
 const paths = require('../../paths');
 const got = require('got');
 
-describe('Download tests', () => {
+describe('Download and static routes tests', () => {
 
-  const downloadURL = `http://localhost:3001${paths.download}?uuid=de741848-5e90-4c5e-8699-78aca9b37aba&filename=test.txt`;
+  const downloadURL = `http://localhost:3001/download?uuid=de741848-5e90-4c5e-8699-78aca9b37aba&filename=test.txt`;
+  const staticURL = 'http://localhost:3001/images/test.jpeg';
 
   test('Download', async () => {
     const response = await got.get(downloadURL);
@@ -12,7 +13,7 @@ describe('Download tests', () => {
   });
 
   test('Static files', async () => {
-    const response = await got.get('http://localhost:3001/images/test.jpeg');
+    const response = await got.get(staticURL);
     expect(response.statusCode).toBe(200);
   });
 
