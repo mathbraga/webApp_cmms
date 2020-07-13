827063cde3a78e6725c170367b972cb6cb1f87f0
hzlopes <hzlopes@gmail.com>
2020-07-13 14:27:38 -0300

    Move env log to server.listen callback

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
 const port = process.env.HTTP_PORT;
-console.log(process.env);
 
 // Cron jobs
 const cronJobs = require('./cron');
 
 // Listen for connections on specified port
-server.listen(port, () => console.log(`Server listening on port ${port}!`));
+server.listen(
+  port,
+  () => {
+    console.log(process.env);
+    console.log(`Server listening on port ${port}!`);
+  }
+);
