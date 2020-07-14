0513b5c395b6b5c8400a3d9194606f59053c55e1
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-07-14 13:30:00 -0300

    Use functional components on message tab

--- frontend/src/views/Task/TabsElements/MessageTab.jsx
+++ frontend/src/views/Task/TabsElements/MessageTab.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PaneTitle from '../../../components/TabPanes/PaneTitle';
 import AnimateHeight from 'react-animate-height';
 import MessageInput from '../../../components/NewForms/MessageInput';
@@ -6,69 +6,58 @@ import MessageBox from '../../../components/Message/MessageBox';
 
 import messages from '../utils/messageTab/fakeMessages';
 
-class MessageTab extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      messageInputOpen: false
-    }
-    this.toggleMessageInput = this.toggleMessageInput.bind(this);
+function MessageTab(props) { 
+  const [ messageInputOpen, setMessageInputOpen ] = useState(false);
+  
+  console.log("Message Props: ", props);
+
+  const actionButtons = {
+    messageInputOpen: [
+      {name: 'Cancelar', color: 'danger', onClick: toggleMessageInput}
+    ],
+    noFormOpen: [
+      {name: 'Nova Mensagem', color: 'success', onClick: toggleMessageInput},
+    ],
+  };
+
+  const heightMessageInput = messageInputOpen === true ? 'auto' : 0;
+  
+  function toggleMessageInput() {
+    setMessageInputOpen(!messageInputOpen);
   }
 
-
-  toggleMessageInput() {
-    this.setState(prevState => ({
-      messageInputOpen: !prevState.messageInputOpen,
-    }));
-  }
-
-  render() {
-    const { messageInputOpen } = this.state;
-
-    const actionButtons = {
-      messageInputOpen: [
-        {name: 'Cancelar', color: 'danger', onClick: this.toggleMessageInput}
-      ],
-      noFormOpen: [
-        {name: 'Nova Mensagem', color: 'success', onClick: this.toggleMessageInput},
-      ],
-    };
-
-    const heightMessageInput = messageInputOpen === true ? 'auto' : 0;
-
-    return (
-      <div className="tabpane-container">
+  return (
+    <div className="tabpane-container">
+      <PaneTitle 
+        actionButtons={messageInputOpen ? actionButtons.messageInputOpen : actionButtons.noFormOpen}
+        title={messageInputOpen ? 'Escrever mensagem' : 'Mensagens'}
+      />
+      <AnimateHeight 
+        duration={300}
+        height={heightMessageInput}
+      >
+        <div className="tabpane__content">
+          <MessageInput 
+            toggleForm={toggleMessageInput}
+          />
+        </div>
+      </AnimateHeight>
+      {(messageInputOpen) && (
         <PaneTitle 
-          actionButtons={messageInputOpen ? actionButtons.messageInputOpen : actionButtons.noFormOpen}
-          title={messageInputOpen ? 'Escrever mensagem' : 'Mensagens'}
+          title={'Mensagens'}
         />
-        <AnimateHeight 
-          duration={300}
-          height={heightMessageInput}
-        >
-          <div className="tabpane__content">
-            <MessageInput 
-              toggleForm={this.toggleMessageInput}
+      )}
+      {
+        messages.map(message => (
+          <div className="tabpane__content" style={{ marginTop: '40px' }}>
+            <MessageBox 
+              message={message}
             />
           </div>
-        </AnimateHeight>
-        {(messageInputOpen) && (
-          <PaneTitle 
-            title={'Mensagens'}
-          />
-        )}
-        {
-          messages.map(message => (
-            <div className="tabpane__content" style={{ marginTop: '40px' }}>
-              <MessageBox 
-                message={message}
-              />
-            </div>
-          ))
-        }
-      </div>
-    );
-  }
+        ))
+      }
+    </div>
+  );
 }
 
 export default MessageTab;
