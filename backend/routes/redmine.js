36a62034c49e6f19d8592240537557e505690e86
hzlopes <hzlopes@gmail.com>
2020-07-20 13:48:56 -0300

    Add redmine variables to env file

--- backend/routes/redmine.js
+++ backend/routes/redmine.js
@@ -7,8 +7,8 @@ router.get(
   async (req, res, next) => {
     const { page } = req.query;
     const { body } = await got.get(
-      `https://redminesf.senado.gov.br/redmine/projects/solicitacoes-de-servico/issues.json?page=${page}&set_filter=1`,
-      { headers: { "X-Redmine-API-Key": '' } },
+      `${process.env.REDMINE_URL}${page}`,
+      { headers: { "X-Redmine-API-Key": process.env.REDMINE_API_KEY } },
     );
     res.json(body);
   }
